3d440306e75034443454caf0462a822f74ee8a20
Ashlyn Knox <ashlyn@lilyx.io>
2022-07-18 16:43:17 -0600

    merge fix after pulling from main

--- client/src/main.ts
+++ client/src/main.ts
@@ -2,7 +2,6 @@ import { createApp } from "vue";
 import App from "@/App.vue";
 
 // plugins
-<<<<<<< HEAD
 import router from "./router";
 import pinia from "./plugins/pinia";
 import head from "./plugins/vueuse-head";
@@ -11,12 +10,6 @@ import i18n from "./plugins/i18n";
 // styles
 import "./assets/css/tailwind.css";
 
-=======
-import router from "@/router";
-import pinia from "@/plugins/pinia";
-import head from "@/plugins/vueuse-head";
-import i18n from "@/plugins/i18n";
->>>>>>> 5f08b267d7efe56737d15dd7506c3787f03a884b
 // use plugins and mount application
 const app = createApp(App);
 
