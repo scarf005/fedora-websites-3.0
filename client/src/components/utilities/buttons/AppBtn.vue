e0ba7829a6f600d47d61bcdd454cd762aade3be7
Ashlyn Knox <ashlyn@lilyx.io>
2022-07-19 15:46:54 -0600

    added tailwind clases to link button using the style tag in Vue SFC

--- client/src/components/utilities/buttons/AppBtn.vue
+++ client/src/components/utilities/buttons/AppBtn.vue
@@ -5,7 +5,13 @@ defineProps({
 </script>
 
 <template>
-  <a :href="url" class="">
+  <a :href="url" class="btn">
     <slot></slot>
   </a>
 </template>
+
+<style lang="postcss">
+.btn {
+  @apply font-medium bg-blue-light hover: bg-blue-light/[0.6] ease-in-out duration-300 hover:ease-in-out max-w-fit rounded py-2 px-4 lg:py-3 lg:px-6;
+}
+</style>
