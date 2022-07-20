30f8f881b725e430adf7d442c9a455af30a5464d
Ashlyn Knox <ashlyn@lilyx.io>
2022-07-20 15:50:17 -0600

    removed styles that are breaking from formatter

--- client/src/components/utilities/buttons/AppBtn.vue
+++ client/src/components/utilities/buttons/AppBtn.vue
@@ -5,13 +5,8 @@ defineProps({
 </script>
 
 <template>
-  <a :href="url" class="btn">
+  <a :href="url" class="bg-blue-light hover:bg-blue-light/[0.6] ease-in-out">
     <slot></slot>
   </a>
 </template>
 
-<style lang="postcss">
-.btn {
-  @apply font-medium bg-blue-light hover: bg-blue-light/[0.6] ease-in-out duration-300 hover:ease-in-out max-w-fit rounded py-2 px-4 lg:py-3 lg:px-6;
-}
-</style>
