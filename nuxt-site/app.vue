3405583cb1e9d3e8150ef7e54f2cea0023332b37
Ashlyn Knox <ashlyn@lilyx.io>
2022-07-20 18:10:43 -0600

    All pages added and set to show their full paths

---
+++ nuxt-site/app.vue
@@ -0,0 +1,26 @@
+<script setup>
+useHead({
+  title: "The Fedora Project",
+  viewport: "width=device-width, initial-scale=1, maximum-scale=1",
+  charset: "utf-8",
+  meta: [
+    {
+      name: "description",
+      content: "Fedora Linux is an open source project. "
+    },
+  ],
+  link: [
+    {
+      rel: "icon",
+      href: "/favicon.ico"
+    }
+  ]
+})
+</script>
+<template>
+  <div id="app">
+    <the-nav-bar />
+    <nuxt-page />
+    <the-footer />
+  </div>
+</template>
---
+++ nuxt-site/pages/start.vue
@@ -0,0 +1,4 @@
+<template>
+  <p>{{ $route.fullPath }}</p>
+</template>
+<script setup></script>
