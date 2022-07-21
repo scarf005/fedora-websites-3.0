72313f53e052ee979ec0f0571405c86e6c4e9c7e
lilyx <ashlyn@lilyx.io>
2022-07-21 16:24:59 -0600

    switched to font-awesome package

--- nuxt-site/nuxt.config.ts
+++ nuxt-site/nuxt.config.ts
@@ -2,15 +2,16 @@ import { defineNuxtConfig } from "nuxt";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  css: [
+    "@fortawesome/fontawesome-svg-core/styles.css"
+  ],
   components: [
     "~/components/",
     "~/components/layout",
     "~/components/ui",
     "~/components/utilities",
   ],
-  buildModules: [
-    "@pinia/nuxt"
-  ],
+  buildModules: ["@pinia/nuxt"],
   // TODO: Finish configuring Strapi to set up with this repo
   modules: ["@nuxtjs/tailwindcss", /* "@nuxtjs/strapi" */],
   // Plugin Configurations
