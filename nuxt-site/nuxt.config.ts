740579b73cb22ebf969b135e14079a295622a990
lilyx <ashlyn@lilyx.io>
2022-07-21 16:24:59 -0600

    switched to font-awesome package

--- nuxt-site/nuxt.config.ts
+++ nuxt-site/nuxt.config.ts
@@ -2,6 +2,9 @@ import { defineNuxtConfig } from "nuxt";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  css: [
+    "@fortawesome/fontawesome-svg-core/styles.css"
+  ],
   components: [
     "~/components/",
     "~/components/layout",
