9811ae5d1a27ab21d5b6b83e5c0f7a4ca57b2b4c
Ashlyn Knox <ashlyn@lilyx.io>
2022-07-26 17:18:16 -0600

    demonstration of loading content onto page

--- nuxt-site/pages/editions/workstation/index.vue
+++ nuxt-site/pages/editions/workstation/index.vue
@@ -1,6 +1,14 @@
 <script setup>
-// TODO: Set up to read information from /content/editions/workstation/index.yml
+// pull data from the content directory like this
+const { data } = await useAsyncData('page-data', () => {
+  return queryContent("editions/workstation/").find()
+})
 </script>
 <template>
-  <p>{{ $route.fullPath }}</p>
+  <!-- add content to the page like this -->
+  <main>
+    <h1>{{ data[0].hero.title }}</h1>
+    <p>{{ data[0].hero.description }}</p>
+    <img :src="`../../../assets/${data[0].hero.image}`" alt="We need to clean up this path issue" class="mt-8" />
+  </main>
 </template>
