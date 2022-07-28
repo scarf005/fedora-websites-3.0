12b26a1ecbdabb12713cfdbe487df34a31909813
niko <n.dunkel@gmail.com>
2022-07-28 13:52:37 -0700

    working

--- nuxt-site/pages/editions/iot/index.vue
+++ nuxt-site/pages/editions/iot/index.vue
@@ -1,13 +1,13 @@
 <script setup>
   // pull data from the content directory like this
   const { data } = await useAsyncData('page-data', () => {
-    return queryContent("blog/foo").find()
+    return queryContent("blog/bar").find()
   })
 </script>
 <template>
   <!-- add content to the page like this -->
   <main>
     <p>{{ data[0].title }}</p>
-    <!-- <img :src="`${$config.app.baseURL +'/'+ data[0].heroImage.replace('public/', '')}`" :alt="data[0].name" class="mt-8" /> -->
+    <img :src="`${$config.app.baseURL +'/'+ data[0].hero.replace('public/', '')}`" :alt="data[0].name" class="mt-8" />
   </main>
 </template>
