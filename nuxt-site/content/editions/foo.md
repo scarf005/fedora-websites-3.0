9c1c1e48518136ddb4f643eb4742c832e3e89fb4
niko <n.dunkel@gmail.com>
2022-07-28 14:02:42 -0700

    test

---
+++ nuxt-site/content/editions/foo.md
@@ -0,0 +1,7 @@
+---
+title: foo
+date: 2022-07-28T20:20:05.343Z
+description: bar
+hero: public/assets/images/iot.png
+---
+bazaz
--- nuxt-site/pages/editions/iot/index.vue
+++ nuxt-site/pages/editions/iot/index.vue
@@ -1,7 +1,7 @@
 <script setup>
   // pull data from the content directory like this
   const { data } = await useAsyncData('page-data', () => {
-    return queryContent("blog/bar").find()
+    return queryContent("editions/foo").find()
   })
 </script>
 <template>
