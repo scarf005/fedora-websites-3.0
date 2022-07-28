eb317a123965ef990c95d7dd4022f1eba9172c43
Ashlyn Knox <ashlyn@lilyx.io>
2022-07-28 12:21:11 -0600

    Working CMS Proof of Concept

--- nuxt-site/pages/editions/iot/index.vue
+++ nuxt-site/pages/editions/iot/index.vue
@@ -1,4 +1,13 @@
-<script setup></script>
+<script setup>
+// pull data from the content directory like this
+const { data } = await useAsyncData('page-data', () => {
+  return queryContent("editions/iot/").find()
+})
+</script>
 <template>
-  <p>{{ $route.fullPath }}</p>
+  <!-- add content to the page like this -->
+  <main>
+    <p>{{ data[0].title }}</p>
+    <img :src="`${$config.app.baseURL + data[0].heroImage}`" :alt="data[0].name" class="mt-8" />
+  </main>
 </template>
--- nuxt-site/pages/editions/workstation/index.vue
+++ nuxt-site/pages/editions/workstation/index.vue
@@ -7,8 +7,7 @@ const { data } = await useAsyncData('page-data', () => {
 <template>
   <!-- add content to the page like this -->
   <main>
-    <h1>{{ data[0].hero.title }}</h1>
-    <p>{{ data[0].hero.description }}</p>
-    <img :src="`../../../assets/${data[0].hero.image}`" alt="We need to clean up this path issue" class="mt-8" />
+    <p>{{ data[0].title }}</p>
+    <!--    <img :src="`${data[0].hero.image}`" alt="We need to clean up this path issue" class="mt-8" /> -->
   </main>
 </template>
