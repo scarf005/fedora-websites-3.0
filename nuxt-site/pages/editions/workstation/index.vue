29220888e4442c49a39d155c89acdb166092a3e9
niko <n.dunkel@gmail.com>
2022-07-28 12:41:58 -0700

    reset old files

--- nuxt-site/pages/editions/workstation/index.vue
+++ nuxt-site/pages/editions/workstation/index.vue
@@ -1,13 +1,4 @@
-<script setup>
-// pull data from the content directory like this
-const { data } = await useAsyncData('page-data', () => {
-  return queryContent("editions/workstation/").find()
-})
-</script>
+<script setup></script>
 <template>
-  <!-- add content to the page like this -->
-  <main>
-    <p>{{ data[0].title }}</p>
-    <!--    <img :src="`${data[0].hero.image}`" alt="We need to clean up this path issue" class="mt-8" /> -->
-  </main>
+  <p>{{ $route.fullPath }}</p>
 </template>
