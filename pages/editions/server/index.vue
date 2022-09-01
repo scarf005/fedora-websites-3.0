ed516cc07fc0d3941e28fa0cab6f92ddcded8bed
niko <n.dunkel@gmail.com>
2022-09-01 10:24:21 -0700

    put initial server placeholder in there

--- pages/editions/server/index.vue
+++ pages/editions/server/index.vue
@@ -7,5 +7,16 @@ useHead({
 });
 </script>
 <template>
-  <p>{{ data }}</p>
+  <FpHero :background="data[0].header.images.backgroundImage" alignment="bg-bottom">
+    <FpBanner
+      :title="data[0].header.title"
+      :subtitle="data[0].header.subtitle"
+      :image="data[0].header.images.image"
+      color="text-fp-green"
+      border="border border-fp-green"
+      background="text-white bg-fp-green"
+      :cta1="data[0].header.cta[0].text"
+      :link1="data[0].header.cta[0].url"
+    />
+  </FpHero>
 </template>
