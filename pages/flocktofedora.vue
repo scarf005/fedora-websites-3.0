e7b4f666a8e8c6916ddce0dc8570873349fea7a3
Francois Andrieu <darknao@fedoraproject.org>
2022-09-05 00:09:03 +0200

    add placeholder sections to Flock page

--- pages/flocktofedora.vue
+++ pages/flocktofedora.vue
@@ -20,7 +20,7 @@ const { data } = await useAsyncData("page-data", () => {
       background="text-white bg-fp-purple"
       :ctas="data[0].header.cta"
     >
-      <h2 class="text-white mt-3 font-semibold">
+      <h2 class="text-white my-8 font-semibold">
         {{ data[0].header.eventDate }}
       </h2>
     </FpBanner>
@@ -39,9 +39,75 @@ const { data } = await useAsyncData("page-data", () => {
   </FpHero>
 
   <main class="flex flex-col items-center mt-8">
+
+    <!-- TODO: Explore -->
+    <section class="w-10/12 mx-auto my-10">
+      <h2
+        class="text-5xl text-center mb-12 font-bold bg-clip-text text-transparent bg-gradient-to-r from-fp-green to-fp-blue-light"
+      >
+        Explore the latest in Open Source
+      </h2>
+    </section>
+
+    <!-- TODO: Hybrid Experience -->
+    <section class="w-10/12 mx-auto my-10">
+      <h2
+        class="text-5xl text-center mb-12 font-bold bg-clip-text text-transparent bg-gradient-to-r from-fp-green to-fp-blue-light"
+      >
+        A Hybrid Experience
+      </h2>
+    </section>
+
+    <!-- TODO: Important Dates -->
+    <section class="w-10/12 mx-auto my-10">
+      <h2
+        class="text-5xl text-center mb-12 font-bold bg-clip-text text-transparent bg-gradient-to-r from-fp-green to-fp-blue-light"
+      >
+        Important Dates
+      </h2>
+    </section>
+
+    <!-- TODO: Event Calendar -->
+    <section class="w-10/12 mx-auto my-10">
+      <h2
+        class="text-5xl text-center mb-12 font-bold bg-clip-text text-transparent bg-gradient-to-r from-fp-green to-fp-blue-light"
+      >
+        Event Calendar
+      </h2>
+    </section>
+
     <!-- Community Section -->
-    <section>
+    <section class="my-10">
       <FpCommunity :data="data[0].section[0]" />
     </section>
+
+    <!-- TODO: Our Sponsors -->
+    <section class="w-10/12 mx-auto my-10">
+      <h2
+        class="text-5xl text-center mb-12 font-bold bg-clip-text text-transparent bg-gradient-to-r from-fp-green to-fp-blue-light"
+      >
+        Our Sponsors
+      </h2>
+    </section>
+
+    <!-- Benefits of Sponsoring -->
+    <section class="w-10/12 my-10">
+      <header
+        class="grid md:grid-cols-2 gap-4 md:w-11/12 mx-auto text-center md:text-left"
+      >
+        <h3
+          class="text-fp-blue font-medium mb-4 xl:mb-6 md:col-span-2 xl:col-span-1"
+        >
+          {{ data[0].section[1].header.sectionTitle }}
+        </h3>
+      </header>
+      <FpList columns="sm:grid-cols-2 gap-12 lg:gap-4">
+        <FpListItem
+          v-for="item in data[0].section[1].content.list"
+          v-bind="item"
+        />
+      </FpList>
+    </section>
+
   </main>
 </template>
