182d297282547d85d2a4f30ab2812cb2f7614909
niko <n.dunkel@gmail.com>
2022-09-06 13:33:15 -0700

    done

--- components/ui/TheNavBar.vue
+++ components/ui/TheNavBar.vue
@@ -85,12 +85,6 @@ const languages = [
               <TheNavItem title="Community" :items="community" />
 
               <TheNavItem title="Support" :items="support" />
-              <form>
-                <select v-model="locale">
-                  <option value="en">en</option>
-                  <option value="fr">fr</option>
-                </select>
-              </form>
 
               <TheNavItem title="Languages" :items="languages" />
 
