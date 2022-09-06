5c8a5b126894aa4a0d552d35bfc3c90c40303311
niko <n.dunkel@gmail.com>
2022-09-06 11:43:20 -0700

    small bugfix for navbar relative vs. absolute link

--- components/ui/TheNavBar.vue
+++ components/ui/TheNavBar.vue
@@ -31,14 +31,14 @@ const support = [
 
 const community = [
   { name: "Discussion Board", href: "https://discussion.fedoraproject.org/" },
+  {
+    name: "Flock To Fedora",
+    href: "/flocktofedora",
+  },
   { name: "Join Fedora", href: "#" },
   { name: "Community Blog", href: "#" },
   { name: "Matrix Chat Server", href: "#" },
   { name: "Fedora Account System", href: "#" },
-  {
-    name: "Flock To Fedora",
-    href: "flocktofedora",
-},
 ];
 
 const open = useState("navbaropen", () => false);
