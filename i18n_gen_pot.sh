0ef156192f629c87baac3b161e18c0e82be3baf0
Francois Andrieu <darknao@fedoraproject.org>
2022-09-12 12:55:00 +0200

    flag url & image key type as non-translatable (read-only)

--- i18n_gen_pot.sh
+++ i18n_gen_pot.sh
@@ -3,5 +3,9 @@
 
 for file in $(find content -type f -name \*.en.yml); do 
   mkdir -p $(dirname ${file/content/locales})
-  yaml2po -P -i $file -o $(sed 's/content/locales/;s/.yml/.pot/' <<<$file);
+  output=$(sed 's/content/locales/;s/.yml/.pot/' <<<$file)
+  yaml2po -P -i $file -o $output;
+
+  # Flag url & image keys as read-only
+  sed -i "/^#.*\([uU]rl\|[iI]mage\)$/i #, read-only" $output
 done
