1d7cbcac450cfcf886aa82882ca3d734cc0eeed2
Niko <n.dunkel@gmail.com>
2022-09-14 22:15:49 +0000

    Update CI files to build PO

--- i18n_gen_yml.sh
+++ i18n_gen_yml.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Compile yaml file from po when translation is above $THRESHOLD % completion
+# Generate yaml file from po when translation is above $THRESHOLD % completion
 THRESHOLD=0
 
 for file in $(find locales/ -type f -name \*.po); do
