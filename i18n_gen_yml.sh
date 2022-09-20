8f7024f9a720ba289025807093959f234cf23477
niko <n.dunkel@gmail.com>
2022-09-20 11:46:50 -0700

    updated scripts

--- i18n_gen_yml.sh
+++ i18n_gen_yml.sh
@@ -4,6 +4,6 @@ THRESHOLD=0
 
 for file in $(find locales/ -type f -name \*.po); do
   echo compiling ${file}..
-  po2yaml -t $(dirname ${file/locales/content})/index.en.yml -i $file -o $(sed 's/locales/content/;s/.po/.yml/' <<<$file) --threshold=$THRESHOLD;
+  po2yaml -t $(dirname ${file/locales/content})/index.yml -i $file -o $(sed 's/locales/content/;s/.po/.yml/' <<<$file) --threshold=$THRESHOLD;
 done
 
--- i18n_recover_yml.sh
+++ i18n_recover_yml.sh
@@ -2,5 +2,5 @@
 # Recover po from already translated yaml files
 
 for file in $(find content/ -type f | grep -v en.yml); do 
-  yaml2po -t $(dirname $file)/index.en.yml -i $file -o $(sed 's/content/locales/;s/.yml/.po/' <<<$file);
+  yaml2po -t $(dirname $file)/index.yml -i $file -o $(sed 's/content/locales/;s/.yml/.po/' <<<$file);
 done
