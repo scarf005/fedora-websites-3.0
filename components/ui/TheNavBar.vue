0853df48b74213a006f0ed1ee33290264baa4668
Ashlyn Knox <ashlyn@lilyx.io>
2022-10-01 15:16:57 -0600

    mobile nav fix attempt 1 using ref #64

--- components/ui/TheNavBar.vue
+++ components/ui/TheNavBar.vue
@@ -6,6 +6,9 @@ import {
   support,
   community,
 } from "../../config/navigation";
+let showMenu = ref(false);
+const toggleNav = () => (showMenu.value = !showMenu.value)
+
 const open = useState("navbaropen", () => false);
 const switchLocalePath = useSwitchLocalePath();
 const { locales } = useI18n();
@@ -21,32 +24,21 @@ const availableLocales = computed(() => {
   <nav class="fixed z-50 w-full bg-fp-blue">
     <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
       <div class="relative flex h-16 items-center justify-between">
-        <div
-          class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start"
-        >
+        <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
           <div class="flex flex-shrink-0 items-center">
             <a :href="`${$config.app.baseURL + '/'}`">
-              <FpImage
-                class="h-8 w-auto"
-                image="assets/images/fedora_white.png"
-              />
+              <FpImage class="h-8 w-auto" image="assets/images/fedora_white.png" />
             </a>
           </div>
 
           <div class="hidden sm:ml-auto sm:block">
             <div class="flex space-x-4">
-              <a
-                class="inline-flex cursor-pointer items-center rounded px-4 text-sm text-white"
-                href="https://docs.fedoraproject.org/en-US/project/"
-              >
+              <a class="inline-flex cursor-pointer items-center rounded px-4 text-sm text-white"
+                href="https://docs.fedoraproject.org/en-US/project/">
                 <span class="mr-1">{{ $t("About") }}</span>
               </a>
 
-              <TheNavItem
-                :title="$t('Desktops')"
-                :items="desktops"
-                icons="true"
-              />
+              <TheNavItem :title="$t('Desktops')" :items="desktops" icons="true" />
 
               <TheNavItem :title="$t('Server & Cloud')" :items="cloud" />
 
@@ -63,41 +55,16 @@ const availableLocales = computed(() => {
 
         <!-- Mobile menu button-->
         <div class="flex items-center sm:hidden">
-          <button
-            @click="open = !open"
-            type="button"
-            class="inline-flex items-center justify-center rounded-md p-2 text-white"
-          >
-            <svg
-              class="block h-6 w-6"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="2"
-              stroke="currentColor"
-              aria-hidden="true"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M4 6h16M4 12h16M4 18h16"
-              />
+          <button @click="toggleNav" type="button"
+            class="inline-flex items-center justify-center rounded-md p-2 text-white">
+            <svg class="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+              stroke-width="2" stroke="currentColor" aria-hidden="true">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16" />
             </svg>
 
-            <svg
-              class="hidden h-6 w-6"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="2"
-              stroke="currentColor"
-              aria-hidden="true"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M6 18L18 6M6 6l12 12"
-              />
+            <svg class="hidden h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+              stroke-width="2" stroke="currentColor" aria-hidden="true">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
             </svg>
           </button>
         </div>
@@ -105,12 +72,10 @@ const availableLocales = computed(() => {
     </div>
 
     <!-- Mobile menu, show/hide based on menu state. -->
-    <div class="sm:hidden" id="mobile-menu" v-if="open">
+    <div :class="showMenu ? 'flex': 'hidden'" id="mobile-menu">
       <div class="space-y-1 px-2 pt-2 pb-3">
-        <a
-          class="inline-flex cursor-pointer items-center rounded px-3 py-2 text-sm text-white"
-          href="https://docs.fedoraproject.org/en-US/project/"
-        >
+        <a class="inline-flex cursor-pointer items-center rounded px-3 py-2 text-sm text-white"
+          href="https://docs.fedoraproject.org/en-US/project/">
           <span class="mr-1">About</span>
         </a>
       </div>
