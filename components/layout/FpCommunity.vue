64b7666d71064367b7bd5442bd55ad5eaf746d64
niko <n.dunkel@gmail.com>
2022-10-19 23:49:50 -0700

    re-wired iot pages

--- components/layout/FpCommunity.vue
+++ components/layout/FpCommunity.vue
@@ -5,20 +5,17 @@ defineProps({
 </script>
 
 <template>
-  <FpHero :background="data.header.headerImage.image" alignment="bg-top">
+  <FpHero :background="data.images" alignment="bg-top">
     <section class="mx-auto w-10/12 max-w-screen-xl py-24">
       <h2 class="mb-5 text-center text-5xl font-semibold text-fp-blue">
-        {{ data.header.sectionTitle }}
+        {{ data.sectionTitle }}
       </h2>
       <h4 class="mx-14 mb-5 text-fp-blue-dark">
-        {{ data.header.subtitle1 }}
+        {{ data.sectionDescription }}
       </h4>
-      <p class="mx-14 mb-8 text-fp-blue-dark">
-        {{ data.header.subtitle2 }}
-      </p>
 
       <FpList columns="sm:grid-cols-2 gap-12 lg:gap-4">
-        <FpListItem v-for="item in data.content.list" v-bind="item" />
+        <FpListItem v-for="item in data.content" v-bind="item" />
       </FpList>
     </section>
   </FpHero>
--- pages/editions/iot/index.vue
+++ pages/editions/iot/index.vue
@@ -38,53 +38,53 @@ useHead({
       </FpBanner>
     </FpHero>
 
-    <!-- <FpHero
-      :background="data.section[0].header.headerImage.image"
+    <FpHero
+      :background="data.sections[0].images"
       alignment="bg-bottom"
     >
       <section class="mx-auto w-10/12 pt-12">
         <h3
           class="mb-4 font-medium text-fp-blue md:col-span-2 xl:col-span-1 xl:mb-6"
         >
-          {{ data.section[0].header.sectionTitle }}
+          {{ data.sections[0].sectionTitle }}
         </h3>
         <FpList columns="sm:grid-cols-2 gap-12 lg:gap-4">
           <FpListItem
-            v-for="item in data.section[0].content.list"
+            v-for="item in data.sections[0].content"
             v-bind="item"
           />
         </FpList>
       </section>
     </FpHero>
-    <div class="bg-magenta-100" style="min-height: 60vh">
+  <div class="bg-magenta-100" style="min-height: 60vh">
       <section class="mx-auto w-10/12 pt-12">
         <h3
           class="mb-4 text-center font-medium text-fp-blue md:col-span-2 xl:col-span-1 xl:mb-6"
         >
-          {{ data.section[1].header.sectionTitle }}
+          {{ data.sections[1].sectionTitle }}
         </h3>
         <FpList columns="sm:grid-cols-4 gap-12 lg:gap-4" disableDots="true">
           <FpListItem
-            v-for="item in data.section[1].content.list"
+            v-for="item in data.sections[1].content"
             v-bind="item"
             images="true"
           />
         </FpList>
       </section>
-    </div> -->
+    </div>
 
     <!-- Community Section -->
-    <!-- <section>
-      <FpCommunity :data="data.section[2]" />
-    </section> -->
+    <section>
+      <FpCommunity :data="data.sections[2]" />
+    </section>
 
     <!-- Call To Action -->
-    <!-- <section>
+    <section>
       <FpCallToAction
-        :cta="data.header.cta"
+        :cta="data.links"
         image="assets/images/fiot-logo.png"
       />
-    </section> -->
+    </section> 
     
   </main>
 </template>
