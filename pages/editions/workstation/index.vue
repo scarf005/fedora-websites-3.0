e31469344e7ae371a308e299bff66bc5cb492537
Ashlyn Knox <ashlyn@lilyx.io>
2022-11-08 10:40:44 -0700

    ws home hero video css adjustments

--- pages/editions/workstation/index.vue
+++ pages/editions/workstation/index.vue
@@ -38,7 +38,7 @@ useContentHead(data);
             image="assets/images/workstation_framework.png"
           />
           <video
-            class="mt-[310px] w-full px-11 sm:mt-[473px] sm:px-[70px]"
+            class="-mt-[354px] w-full px-11 sm:-mt-[535px] sm:px-[78px]"
             autoplay=""
             loop=""
             muted=""
