10dc63c8ad204b2d3a2c0e00e2b0081d5b5f9050
Ashlyn Knox <ashlyn@lilyx.io>
2022-11-09 16:18:17 -0700

    applied new components to ws community

--- pages/editions/iot/community.vue
+++ pages/editions/iot/community.vue
@@ -73,7 +73,9 @@ useContentHead(data);
       <div
         class="container mx-auto grid justify-center gap-8 py-12 lg:grid-cols-2 lg:justify-start xl:py-8"
       >
-        <header class="col-span-full my-8 mx-auto text-center lg:text-start">
+        <header
+          class="col-span-full my-8 mx-auto w-full text-center md:text-start"
+        >
           <h2 class="text-fp-purple xl:text-4xl">
             {{ data.sections[2].sectionTitle }}
           </h2>
@@ -81,11 +83,13 @@ useContentHead(data);
         <div
           v-for="content in data.sections[2].content"
           :key="content.id"
-          class="mx-auto max-w-lg text-center lg:text-start"
+          class="max-w-lg text-center lg:text-start"
         >
           <h3 class="font-medium text-fp-blue">{{ content.title }}</h3>
           <p class="max-w-sm text-fp-gray-darkest">{{ content.description }}</p>
         </div>
+
+        <FpJoinTip />
       </div>
     </section>
     <!-- Fedora Events -->
