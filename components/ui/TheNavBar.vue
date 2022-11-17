dd84b9ed66ca76849fb4519d76f303947436532e
niko <n.dunkel@gmail.com>
2022-11-16 22:45:56 -0800

    fix links

--- components/ui/TheNavBar.vue
+++ components/ui/TheNavBar.vue
@@ -20,26 +20,22 @@ const availableLocales = computed(() => {
           class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start"
         >
           <div class="flex flex-shrink-0 items-center">
-            <a
-              :href="`${
-                $config.app.baseURL.replace(new RegExp('/$'), '') + '/'
-              }`"
-            >
+            <NuxtLink to="/">
               <FpImage
                 class="h-8 w-auto"
                 image="assets/images/fedora_white.png"
               />
-            </a>
+            </NuxtLink>
           </div>
 
           <div class="hidden sm:ml-auto sm:block">
             <div class="flex space-x-4">
-              <a
+              <NuxtLink
                 class="inline-flex cursor-pointer items-center rounded px-4 text-sm text-white"
-                :href="about"
+                :to="about"
               >
                 <span class="mr-1">{{ $t("About") }}</span>
-              </a>
+              </NuxtLink>
 
               <TheNavItem
                 :title="$t('Editions')"
