7d7c133fa582d668947fe020429bb080e486374a
Jefferson Oliveira <jefferson.dev.insights@gmail.com>
2022-11-17 07:57:20 -0300

    tested platforms coreos

--- pages/editions/coreos/index.vue
+++ pages/editions/coreos/index.vue
@@ -49,4 +49,21 @@ useContentHead(data);
       </FpList>
     </section>
   </FpHero>
+
+  <div class="bg-magenta-100" style="min-height: 60vh">
+    <section class="mx-auto w-10/12 pt-12">
+      <h3
+        class="mb-4 text-center font-medium text-fp-blue md:col-span-2 xl:col-span-1 xl:mb-6"
+      >
+        {{ data.sections[1].sectionTitle }}
+      </h3>
+      <FpList columns="sm:grid-cols-4 gap-12 lg:gap-4" :disableDots="true">
+        <FpListItem
+          v-for="item in data.sections[1].content"
+          v-bind="item"
+          images="true"
+        />
+      </FpList>
+    </section>
+  </div>
 </template>
