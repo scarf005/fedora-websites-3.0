7fd3590556c1a59aab7288026f45552ff1853e64
niko <n.dunkel@gmail.com>
2022-11-18 08:57:54 -0800

    initial fplink

--- components/utilities/FpLink.vue
+++ components/utilities/FpLink.vue
@@ -1,11 +1,20 @@
 <script setup>
 defineProps({
   url: String,
+  current: Boolean,
 });
 </script>
 
 <template>
-  <a :href="url">
+  <a
+    :href="`${
+      url.includes('https')
+        ? url
+        : $config.app.baseURL.replace(new RegExp('/$'), '') + url
+    }`"
+    class="rounded-md px-3 py-4 text-base font-medium text-gray-500 hover:text-fp-purple dark:text-gray-300"
+    :aria-current="current ? 'page' : undefined"
+  >
     <slot></slot>
   </a>
 </template>
