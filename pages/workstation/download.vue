bf33f5fdb7972ac7f5ef79a87a8b5e74a3880bba
niko <n.dunkel@gmail.com>
2022-12-13 11:48:34 -0800

    t

--- pages/workstation/download.vue
+++ pages/workstation/download.vue
@@ -52,12 +52,29 @@ useContentHead(data);
           </div>
         </div>
       </section>
-      <template v-for="(cta, idx) in data.links">
-        <FpLink :href="cta.url" class="mx-5 text-blue-500">
+      <div class="flex">
+        <FpLink
+          :href="data.sections[0].content[2].link.url"
+          class="mx-5 text-blue-500"
+        >
           <Icon name="fa-book" />
-          {{ cta.text }}
+          {{ data.sections[0].content[2].title }}
         </FpLink>
-      </template>
+        <FpLink
+          :href="data.sections[0].content[3].link.url"
+          class="mx-5 text-blue-500"
+        >
+          <Icon name="fa-book" />
+          {{ data.sections[0].content[3].title }}
+        </FpLink>
+        <FpLink
+          :href="data.sections[0].content[4].link.url"
+          class="mx-5 text-blue-500"
+        >
+          <Icon name="fa-book" />
+          {{ data.sections[0].content[4].title }}
+        </FpLink>
+      </div>
 
       <!-- FEDORA MEDIA WRITER -->
       <div class="my-10 grid grid-cols-2">
@@ -72,15 +89,19 @@ useContentHead(data);
             <p class="mb-10 text-fp-gray">
               {{ data.sections[1].sectionDescription }}
             </p>
-
-            <div class="flex">
-              <FpLink
-                :href="item.link.url"
-                v-for="item in data.sections[1].content"
-                class="rounded-xl border border-blue-400 px-6 py-3 text-blue-400"
-              >
-                <Icon name="fa-download" /> {{ item.link.text }}
-              </FpLink>
+            <div
+              class="flex items-center justify-between rounded-xl border border-gray-200 bg-white p-5"
+            >
+              <p>Fedora Media Writer</p>
+              <div>
+                <FpLink
+                  :href="item.link.url"
+                  v-for="item in data.sections[1].content"
+                  class="rounded-xl border border-blue-400 px-6 py-3 text-blue-400"
+                >
+                  <Icon :name="item.link.text" />
+                </FpLink>
+              </div>
             </div>
           </div>
         </div>
@@ -116,7 +137,7 @@ useContentHead(data);
         </div>
       </div>
 
-      <!-- OTHER SECTIONS -->
+      <!-- SECURITY -->
       <div class="my-10 flex">
         <div class="flex-1">
           <h2 class="text-fp-blue">
@@ -141,28 +162,44 @@ useContentHead(data);
           }}</FpLink>
         </div>
       </div>
-      <div class="my-10">
-        <h2 class="text-fp-blue">
-          {{ data.sections[4].sectionTitle }}
-        </h2>
-        <p class="text-fp-gray">
-          {{ data.sections[4].sectionDescription }}
-        </p>
-        <FpLink :href="data.sections[4].content[0].link.url">{{
-          data.sections[4].content[0].link.text
-        }}</FpLink>
+
+      <!-- LAPTOPS PRELOADED -->
+      <div class="my-10 flex">
+        <div class="flex-1 p-5">
+          <FpImage :src="data.sections[4].content[0].image" />
+        </div>
+        <div class="flex-1 p-5">
+          <h2 class="text-fp-blue">
+            {{ data.sections[4].sectionTitle }}
+          </h2>
+          <p class="text-fp-gray">
+            {{ data.sections[4].sectionDescription }}
+          </p>
+          <FpLink :href="data.sections[4].content[0].link.url">{{
+            data.sections[4].content[0].link.text
+          }}</FpLink>
+        </div>
       </div>
-      <div class="my-10">
-        <h2 class="text-fp-blue">
-          {{ data.sections[5].sectionTitle }}
-        </h2>
-        <p class="text-base text-fp-gray">
-          {{ data.sections[5].content[0].description }}
-        </p>
-        <p class="text-xs text-gray-300">
-          {{ data.sections[5].content[1].description }}
-        </p>
+
+      <!-- LEARN MORE ABOUT FEDORA MEDIA WRITER -->
+      <div class="my-10 flex">
+        <div class="flex-1 p-5">
+          <h2 class="text-fp-blue">
+            {{ data.sections[5].sectionTitle }}
+          </h2>
+          <p class="text-base text-fp-gray">
+            {{ data.sections[5].content[0].description }}
+          </p>
+          <p class="text-xs text-gray-300">
+            {{ data.sections[5].content[1].description }}
+          </p>
+        </div>
+        <div class="flex-1 p-5">
+          <FpImage :src="data.sections[5].content[1].image" />
+        </div>
       </div>
+
+      <!-- CONTRIBUTE -->
       <div class="my-10">
         <h2 class="text-fp-blue">
           {{ data.sections[6].sectionTitle }}
@@ -170,18 +207,27 @@ useContentHead(data);
         <p class="text-base text-fp-gray">
           {{ data.sections[6].sectionDescription }}
         </p>
-        <div v-for="item in data.sections[6].content">
-          <FpLink href="item.link.url">{{ item.title }}</FpLink>
+        <div class="flex justify-between">
+          <FpLink
+            v-for="item in data.sections[6].content"
+            href="item.link.url"
+            >{{ item.title }}</FpLink
+          >
         </div>
 
         <p class="text-base text-fp-gray">
           {{ data.sections[7].sectionTitle }}
         </p>
-        <div v-for="item in data.sections[7].content">
-          <FpLink href="item.link.url">{{ item.title }}</FpLink>
+        <div class="flex justify-between">
+          <FpLink
+            v-for="item in data.sections[7].content"
+            href="item.link.url"
+            >{{ item.title }}</FpLink
+          >
         </div>
       </div>
 
+      <!-- COMPLIANCE -->
       <div class="my-10">
         <h2 class="text-fp-blue">
           {{ data.sections[8].sectionTitle }}
