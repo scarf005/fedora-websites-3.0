9964da8249a38983b3cba6c4e66d19189bfc76ff
Ashlyn Knox <ashlyn@lilyx.io>
2022-12-12 18:46:04 -0700

    mobile first styling

--- components/ui/TheNav.vue
+++ components/ui/TheNav.vue
@@ -2,10 +2,13 @@
 const { data } = await useAsyncData("navigation", () =>
   queryContent("_navigation").findOne()
 );
-const downloads = data._value.downloads;
-const community = data._value.community;
-const contributors = data._value.contributors;
-const support = data._value.support;
+
+const categories = {
+  downloads: data._value.downloads,
+  community: data._value.community,
+  contributors: data._value.contributors,
+  support: data._value.support,
+};
 </script>
 <template>
   <nav
@@ -13,23 +16,36 @@ const support = data._value.support;
   >
     <!-- layout div-->
     <div class="flex items-center justify-between gap-4 px-6 py-2 lg:py-4">
-      <!-- Logo-->
-      <div class="flex items-center gap-2 text-white">
+      <!-- Logo -->
+      <NuxtLink to="/" class="flex items-center gap-2 text-white">
         <Icon name="fa6-brands:fedora" size="48" />
-        <!-- render fedora on desktop and take page category on mobile-->
-        <span class="font-sans text-2xl font-semibold lg:text-3xl">Fedora</span>
-      </div>
+        <!-- This needs to extract from the metadata-->
+        <p class="text-2xl font-medium">Fedora</p>
+      </NuxtLink>
       <!-- Navigation Links -->
-      <div>
-        <ul></ul>
-      </div>
-
       <!-- Hamburger Toggle-->
-      <div class="text-white md:hidden">
+      <div class="justify-self-end text-white md:hidden">
         <button @click.prevent="">
           <Icon name="fa6-solid:bars" class="text-xl" size="32" />
         </button>
       </div>
+      <!-- Category Menu -->
+      <div class="hidden lg:block">
+        <ul class="flex gap-3">
+          <li
+            v-for="category in categories"
+            :key="category.id"
+            class="text-lg font-medium text-white lg:text-xl xl:text-2xl"
+          >
+            <!--  -->
+            <button class="">
+              <span>{{ category.icon }}</span>
+              <span>{{ category.label }}</span>
+            </button>
+          </li>
+        </ul>
+      </div>
+      <!-- Language Selector -->
     </div>
   </nav>
 </template>
