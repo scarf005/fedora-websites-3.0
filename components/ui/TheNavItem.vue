9e7aee60745122f484c2bc2a70a81167f0f9dce1
Ashlyn Knox <ashlyn@lilyx.io>
2022-12-23 12:03:52 -0700

    disabled mobile menu dropdowns at desktop size

--- components/ui/TheNavItem.vue
+++ components/ui/TheNavItem.vue
@@ -5,13 +5,19 @@ defineProps({
   },
 });
 const mobileMenuToggle = ref(false);
+
+function menuToggle() {
+  if (window.innerWidth < 1024) {
+    mobileMenuToggle.value = !mobileMenuToggle.value;
+  }
+}
 </script>
 <template>
   <!-- Use onMounted() approach to switch function between showing the mobile and showing the desktop layouts -->
   <div
     class="mx-2 mt-4 flex cursor-pointer justify-between rounded-md py-4 px-2 text-white duration-150 ease-in-out hover:bg-fp-blue-dark lg:py-2"
     role="button"
-    @click.prevent="mobileMenuToggle = !mobileMenuToggle"
+    @click.prevent="menuToggle"
   >
     <h3 class="font-medium">
       {{ itemData.label }}
