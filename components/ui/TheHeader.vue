b15badaa35e69acaeb211ab007f2e0bf707148c3
Ashlyn Knox <ashlyn@lilyx.io>
2022-12-30 18:28:55 -0700

    removed lifecyclehook and setup basic top level functionality to work again

--- components/ui/TheHeader.vue
+++ components/ui/TheHeader.vue
@@ -4,7 +4,6 @@ import { onMounted } from "vue";
 const { data } = await useAsyncData("navigation", () =>
   queryContent("_navigation").findOne()
 );
-
 const categories = {
   downloads: data._value.downloads,
   community: data._value.community,
@@ -12,41 +11,23 @@ const categories = {
   support: data._value.support,
 };
 
-function onResize() {
-  showMobile.value = window.innerWidth > 1024;
-  if (window.innerWidth > 1024) {
-    showMenu.value = false;
-  }
-}
+// Menu Toggles
+const showMobileMenu = useState("mobileMenu", () => false);
+const showNavMenu = useState("navMenu", () => false);
 
-onMounted(() => {
-  onResize();
-  window.addEventListener("resize", onResize, { passive: true });
-});
+const toggleMobileMenu = () => {
+  showMobileMenu.value = !showMobileMenu.value;
+  showNavMenu.value = !showNavMenu.value;
+};
+const toggleNavMenu = () => {
+  return (showNavMenu.value = !showNavMenu.value);
+};
 
-function mobileToggle() {
-  showMobile.value = !showMobile.value;
-  showMenu.value = !showMenu.value;
-}
-
-function menuToggle(mobile = false) {
-  if (mobile == true) {
-    showMobile.value = !showmobile.value;
-  }
-  showMenu.value = !showMenu.value;
-}
-
-let showMobile = useState("mobileToggle", () => false);
-
-let showMenu = useState("menuToggle", () => false);
-
-let menuContent = ref({});
-
-function updateMenuContent(obj) {
-  menuContent.value = obj;
-  if (showMenu.value == false) {
-    showMenu.value = true;
-  }
+// Data to pass to right side of the menu
+const menuContent = useState(() => categories.downloads);
+function updateMenuContent(category) {
+  menuContent.value = category;
+  toggleNavMenu();
 }
 </script>
 
@@ -56,15 +37,15 @@ function updateMenuContent(obj) {
   >
     <div class="mx-4 grid grid-cols-2 items-center py-2 lg:pt-4 lg:pb-2">
       <TheHeaderLogo />
-
+      <!-- Switch back to showMobile -->
       <FpNav
-        class="col-span-full flex gap-4 justify-self-center py-4 font-medium text-white lg:col-span-1 lg:justify-self-end"
-        :class="showMobile ? 'block' : 'hidden'"
+        class="col-span-full hidden gap-4 justify-self-center py-4 font-medium text-white lg:col-span-1 lg:flex lg:justify-self-end"
       >
         <button
           v-for="category in categories"
           :key="category.id"
           @click.prevent="updateMenuContent(category)"
+          role="navigation"
         >
           <div class="lg:hidden">
             <Icon :name="category.icon" size="32" />
@@ -72,16 +53,17 @@ function updateMenuContent(obj) {
           <p>{{ category.label }}</p>
         </button>
       </FpNav>
+
       <LazyFpToggle
-        :iconToggle="showMobile"
-        @emitToggle="mobileToggle"
+        :iconToggle="showMobileMenu"
+        @emitToggle="toggleMobileMenu"
         class="col-start-2 row-start-1 justify-self-end lg:hidden"
       />
     </div>
     <TheNav
       :menuContent="menuContent"
-      :class="showMenu ? 'block' : 'hidden'"
-      @emitClose="menuToggle"
+      :class="showNavMenu ? 'block' : 'hidden'"
+      @emitClose="toggleNavMenu"
     />
   </header>
 </template>
