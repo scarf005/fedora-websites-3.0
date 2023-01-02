7d0b9153fbd5e645cf28f5cb45d0ac7f53dc7fb7
niko <n.dunkel@gmail.com>
2023-01-02 14:03:46 -0800

    initial pass done

--- components/ui/TheNav.vue
+++ components/ui/TheNav.vue
@@ -18,7 +18,6 @@ const availableLocales = computed(() => {
   }));
 });
 
-// Menu Toggles
 const categoryOpen = useState("category", () => null);
 const sectionOpen = useState("section", () => null);
 </script>
@@ -27,19 +26,22 @@ const sectionOpen = useState("section", () => null);
   <header
     class="fixed z-50 w-full bg-gradient-to-tr from-fp-blue-light to-fp-blue dark:from-fp-blue dark:to-fp-blue-dark"
   >
+    <!-- NAVBAR  -->
     <div class="mx-4 grid grid-cols-2">
-      <div class="col-span-2 flex justify-between md:col-span-1">
-        <TheNavLogo />
+      <div
+        class="col-span-2 mt-2 flex items-center justify-center md:col-span-1 md:mt-0 md:justify-start"
+      >
+        <FpNavLogo />
       </div>
 
       <FpNav
-        class="col-span-2 flex gap-2 justify-self-end justify-self-center py-4 font-medium text-white md:col-span-1"
+        class="col-span-2 flex justify-center gap-3 py-4 text-white md:col-span-1"
       >
         <button
           v-for="category in categories"
           :key="category.id"
           role="navigation"
-          :class="`rounded-xl p-2 hover:bg-fp-blue-dark  ${
+          :class="`rounded-xl hover:bg-fp-blue-dark md:p-2  ${
             categoryOpen === category && 'md:bg-fp-blue'
           }`"
           @click="
@@ -58,33 +60,41 @@ const sectionOpen = useState("section", () => null);
           <p class="text-sm text-white">{{ category.label }}</p>
         </button>
         <div class="hidden items-center justify-end md:flex">
-          <TheNavThemeSelector />
+          <FpThemeSelector />
         </div>
       </FpNav>
     </div>
 
+    <!-- MENU -->
     <nav
       v-if="categoryOpen"
-      class="left-0 right-0 mx-auto h-screen overflow-scroll bg-fp-blue px-8 md:overflow-hidden lg:absolute lg:h-[40rem] lg:w-11/12 lg:rounded-b-lg lg:shadow-md xl:h-[30rem]"
+      class="left-0 right-0 mx-auto h-screen bg-fp-blue px-8 md:absolute md:h-[40rem] md:w-11/12 md:rounded-b-lg md:shadow-md"
     >
-      <div
-        class="grid h-full gap-8 lg:grid-cols-[minmax(200px,300px)_1fr_180px] lg:grid-rows-[50px_1fr_60px]"
-      >
-        <header class="m-4 hidden w-fit lg:block">
-          <h2 class="text-xl font-semibold uppercase text-white">
-            Fedora {{ categoryOpen.label }}
-          </h2>
-        </header>
+      <!-- close button -->
+      <div class="mt-3 mr-8 hidden items-center justify-end text-white md:flex">
+        <FpLanguageSelector
+          :title="$t('Languages')"
+          :items="availableLocales"
+        />
+        <button @click="categoryOpen = null" class="hover:opacity-75">
+          <Icon name="fa6-solid:x" size="24" />
+        </button>
+      </div>
 
-        <!-- Categories and mobile drop downs -->
-        <section class="col-start-1 w-full">
+      <div class="grid h-full gap-8 lg:grid-cols-12">
+        <section class="col-span-3 w-full">
+          <header class="m-4 hidden w-fit lg:block">
+            <h2 class="text-xl font-semibold uppercase text-white">
+              Fedora {{ categoryOpen.label }}
+            </h2>
+          </header>
           <ul>
             <li
               v-for="section in categoryOpen.sections"
               :key="section.id"
               role="button"
             >
-              <!-- category for both desktop & mobile -->
+              <!-- Category List -->
               <div
                 :class="`mx-2 mt-4 flex cursor-pointer justify-between rounded-md py-4 px-2 text-white duration-150 ease-in-out hover:bg-fp-blue-dark lg:py-2 ${
                   sectionOpen === section && 'md:bg-fp-blue-dark'
@@ -96,18 +106,29 @@ const sectionOpen = useState("section", () => null);
                   {{ section.label }}
                 </h3>
                 <div class="lg:hidden">
-                  <Icon name="fa6-solid:chevron-right" size="32" />
+                  <Icon
+                    name="fa6-solid:chevron-right"
+                    :class="`${
+                      sectionOpen.label === section.label &&
+                      'rotate-90 duration-150 ease-in-out'
+                    }`"
+                    size="32"
+                  />
                 </div>
               </div>
 
               <!-- Sections List Mobile -->
               <ul class="ml-10 block text-lg text-white sm:hidden">
-                <li v-for="link in section.links" :key="link.id" class="py-1">
-                  <FpLink
-                    :href="link.path"
-                    class="underline-offset-4 hover:underline"
-                    >{{ link.label }}</FpLink
-                  >
+                <li
+                  v-if="sectionOpen.label === section.label"
+                  v-for="link in section.links"
+                  :key="link.id"
+                  class="py-1"
+                >
+                  <FpLink :href="link.path">
+                    <Icon :name="link.icon" size="24" class="mr-2" />
+                    {{ link.label }}
+                  </FpLink>
                 </li>
               </ul>
             </li>
@@ -116,7 +137,7 @@ const sectionOpen = useState("section", () => null);
 
         <!-- Section List Desktop -->
         <section
-          class="col-span-2 col-start-2 row-start-1 hidden text-white lg:block"
+          class="col-span-9 hidden text-white lg:block"
           v-if="sectionOpen"
         >
           <div>
@@ -136,6 +157,7 @@ const sectionOpen = useState("section", () => null);
               >
                 <FpLink :href="link.path">
                   <h4 class="mb-2 font-medium text-white">
+                    <Icon :name="link.icon" size="32" />
                     {{ link.label }}
                   </h4>
                   <p class="text-white">
@@ -146,16 +168,6 @@ const sectionOpen = useState("section", () => null);
             </ul>
           </div>
         </section>
-
-        <!-- close button -->
-        <div
-          class="col-start-3 row-start-1 mr-8 hidden items-center justify-end text-white lg:flex"
-        >
-          <button @click="categoryOpen = null" class="hover:opacity-75">
-            <Icon name="fa6-solid:x" size="24" />
-          </button>
-          <NavBarItem :title="$t('Languages')" :items="availableLocales" />
-        </div>
       </div>
     </nav>
   </header>
