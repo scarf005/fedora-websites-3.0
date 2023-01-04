37fb12d58790b66d7df8e39036ef5894a6c6c30c
Ashlyn Knox <ashlyn@lilyx.io>
2023-01-03 17:09:00 -0700

    menu toggle x and better width

--- components/ui/TheNav.vue
+++ components/ui/TheNav.vue
@@ -48,7 +48,9 @@ const sectionOpen = useState("section", () => null);
           type="button"
           class="rounded-md p-2 text-white md:hidden"
         >
-          <Icon name="fa6-solid:bars" />
+          <Icon v-if="categoryOpen == null" name="fa6-solid:bars" />
+
+          <Icon v-else name="fa6-solid:x" />
         </button>
       </div>
 
@@ -89,7 +91,7 @@ const sectionOpen = useState("section", () => null);
     <!-- MENU -->
     <nav
       v-if="categoryOpen"
-      class="left-0 right-0 mx-auto h-screen bg-fp-blue px-8 md:absolute md:h-[40rem] md:w-11/12 md:rounded-b-lg md:shadow-md"
+      class="left-0 right-0 mx-auto h-screen bg-fp-blue px-8 md:absolute md:h-[40rem] md:w-10/12 md:rounded-b-lg md:shadow-md"
     >
       <!-- close button -->
       <div class="mt-3 mr-8 hidden items-center justify-end text-white md:flex">
@@ -139,13 +141,11 @@ const sectionOpen = useState("section", () => null);
               </div>
 
               <!-- Sections List Mobile -->
-              <ul class="ml-10 block text-lg text-white sm:hidden">
-                <li
-                  v-if="sectionOpen.label === section.label"
-                  v-for="link in section.links"
-                  :key="link.id"
-                  class="py-1"
-                >
+              <ul
+                v-if="sectionOpen.label === section.label"
+                class="ml-10 block text-lg text-white sm:hidden"
+              >
+                <li v-for="link in section.links" :key="link.id" class="py-1">
                   <FpLink :href="link.path">
                     <Icon :name="link.icon" size="24" class="mr-2" />
                     {{ link.label }}
