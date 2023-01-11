6610f6b11ea0605358313fd6e0f1f7ebc352646d
Niko <n.dunkel@gmail.com>
2023-01-11 17:15:55 +0000

    Update file TheNav.vue

--- components/ui/TheNav.vue
+++ components/ui/TheNav.vue
@@ -63,8 +63,8 @@ const sectionOpen = useState("section", () => null);
           v-for="category in categories"
           :key="category.id"
           role="navigation"
-          :class="`rounded-xl p-1 hover:bg-fp-blue-light md:p-3 ${
-            categoryOpen === category && 'md:bg-fp-blue-dark'
+          :class="`rounded-xl p-1 hover:bg-fp-blue md:p-3 ${
+            categoryOpen === category && 'bg-fp-blue'
           }`"
           @click="
             if (categoryOpen && category.label === categoryOpen.label) {
