af8a7d01c897dfb82b85a34f85bc95aa74480daf
niko <n.dunkel@gmail.com>
2023-01-13 08:56:36 -0800

    fix up dark light mode selector

--- components/partials/FpThemeSelector.vue
+++ components/partials/FpThemeSelector.vue
@@ -1,31 +1,38 @@
-<script setup></script>
+<script setup>
+const colorMode = useColorMode();
+</script>
+
 <template>
-  <div
-    class="group relative inline-block cursor-pointer rounded-xl hover:bg-fp-blue-dark md:p-3"
-  >
-    <button type="button" id="themeSwitch" class="fill-white stroke-white">
-      <span class="dark:hidden">
-        <svg
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          class="h-6 w-6"
-        >
-          <path d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"></path>
-          <path
-            d="M12 4v1M17.66 6.344l-.828.828M20.005 12.004h-1M17.66 17.664l-.828-.828M12 20.01V19M6.34 17.664l.835-.836M3.995 12.004h1.01M6 6l.835.836"
-          ></path>
-        </svg>
-      </span>
-      <span class="hidden dark:inline">
-        <svg viewBox="0 0 512 512" class="mt-1 h-4 w-6">
-          <path
-            d="M421.6 379.9c-.6641 0-1.35 .0625-2.049 .1953c-11.24 2.143-22.37 3.17-33.32 3.17c-94.81 0-174.1-77.14-174.1-175.5c0-63.19 33.79-121.3 88.73-152.6c8.467-4.812 6.339-17.66-3.279-19.44c-11.2-2.078-29.53-3.746-40.9-3.746C132.3 31.1 32 132.2 32 256c0 123.6 100.1 224 223.8 224c69.04 0 132.1-31.45 173.8-82.93C435.3 389.1 429.1 379.9 421.6 379.9zM255.8 432C158.9 432 80 353 80 256c0-76.32 48.77-141.4 116.7-165.8C175.2 125 163.2 165.6 163.2 207.8c0 99.44 65.13 183.9 154.9 212.8C298.5 428.1 277.4 432 255.8 432z"
-          />
-        </svg>
-      </span>
+  <div class="group relative inline-block">
+    <button
+      type="button"
+      v-if="colorMode.preference === 'system'"
+      @click="colorMode.preference = 'light'"
+      class="hidden fill-white stroke-white sm:block"
+    >
+      <Icon name="fa-solid:laptop" class="white" />
+    </button>
+
+    <button
+      type="button"
+      v-if="colorMode.preference === 'light'"
+      @click="colorMode.preference = 'dark'"
+      class="hidden fill-white stroke-white sm:block"
+    >
+      <Icon name="fa-solid:sun" class="white" />
+    </button>
+
+    <button
+      type="button"
+      v-if="colorMode.preference === 'dark'"
+      @click="colorMode.preference = 'system'"
+      class="hidden fill-white stroke-white sm:block"
+    >
+      <Icon
+        v-if="colorMode.preference === 'dark'"
+        name="fa-solid:moon"
+        class="white"
+      />
     </button>
   </div>
 </template>
