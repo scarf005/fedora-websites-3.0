c6fc894115512a38cf303eb58f5c6435c0dde5bd
niko <n.dunkel@gmail.com>
2023-01-24 13:52:28 -0800

    fix conflict

--- pages/index.vue
+++ pages/index.vue
@@ -59,9 +59,7 @@ onMounted(() => {
         <div>
           <!-- TODO: Add dynamic changing of text in the span -->
           <h1 class="mx-auto mb-8 text-3xl font-semibold md:text-9xl">
-            <<<<<<< HEAD It's your <br /><span>{{ typeValue }} </span>. =======
             It's your <br />&nbsp;<span>{{ typeValue }} </span>
-            >>>>>>> develop
           </h1>
           <p class="mx-auto mb-6 w-4/6 text-3xl">
             An innovative platform for hardware, clouds, and containers, built
