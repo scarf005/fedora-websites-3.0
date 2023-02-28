192a46336bf3678460e6c4b07c4aad014ad21615
Ashlyn Knox <ashlyn@lilyx.io>
2023-02-28 11:59:09 -0700

    more specific setup comments

--- pages/sponsors.vue
+++ pages/sponsors.vue
@@ -1,22 +1,36 @@
 <script setup>
-// import data from cms
+// import data from cms see line 2 of flocktofedora.vue
 </script>
 <template>
   <main>
     <header>
-      <!-- Hero Content -->
+      <div>
+        <!-- h1 Hero Content -->
+      </div>
+      <div>
+        <!-- Red Hat Section -->
+      </div>
     </header>
     <section>
       <header>
-        <!-- Gallery Header -->
+        <!-- h2 General  Sponsor Gallery Header -->
       </header>
       <div>
         <!-- Gallery Content -->
+        <!-- Note: use v-for to loop -->
       </div>
     </section>
     <section>
       <header>
-        <!-- Sponsor Engagement Header -->
+        <!-- h2 Flock Sponsor Gallery Header -->
+      </header>
+      <div>
+        <!-- Gallery Content See line 364 of flocktofedora.vue -->
+      </div>
+    </section>
+    <section>
+      <header>
+        <!-- h2 Sponsor Engagement Header -->
       </header>
       <div>
         <!-- Sponsor Engagement content -->
