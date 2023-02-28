0fb9402fa53b2f8fdf356c4a27d412f87605a4de
Ashlyn Knox <ashlyn@lilyx.io>
2023-02-28 11:32:32 -0700

    general layout and comments

--- pages/sponsors.vue
+++ pages/sponsors.vue
@@ -1,4 +1,26 @@
-<script setup></script>
+<script setup>
+// import data from cms
+</script>
 <template>
-  <main></main>
+  <main>
+    <header>
+      <!-- Hero Content -->
+    </header>
+    <section>
+      <header>
+        <!-- Gallery Header -->
+      </header>
+      <div>
+        <!-- Gallery Content -->
+      </div>
+    </section>
+    <section>
+      <header>
+        <!-- Sponsor Engagement Header -->
+      </header>
+      <div>
+        <!-- Sponsor Engagement content -->
+      </div>
+    </section>
+  </main>
 </template>
