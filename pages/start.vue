65753a149ee037dc134a2bd0672a0832f7e21352
Gregory Bartholomew <gregory.lee.bartholomew@gmail.com>
2023-04-07 14:58:23 -0500

    start: small theme improvements

--- pages/start.vue
+++ pages/start.vue
@@ -103,12 +103,14 @@ if (magazine != null && newsfeed != null) {
 </script>
 
 <template>
-  <FpHero :background="data.header_images[0].image">
-    <div class="mx-auto w-4/5 max-w-7xl md:mt-2">
-      <h2 class="py-4 text-[30px] font-semibold">{{ data.description }}</h2>
+  <FpHero :background="data.header_images[0].image" alignment="mt-2">
+    <div class="mx-auto w-4/5 max-w-7xl pb-6 md:mt-2">
+      <h2 class="py-4 text-[30px] font-semibold text-white dark:text-gray-400">
+        {{ data.description }}
+      </h2>
       <div
         v-for="h in headlines"
-        class="sm:max-h-22 mb-6 flex flex-wrap bg-gray-200 px-4 py-4 dark:bg-neutral-900 sm:flex-nowrap"
+        class="sm:max-h-22 opacity-85 mb-6 flex flex-wrap bg-white px-4 py-4 dark:bg-gray-600 dark:opacity-80 sm:flex-nowrap"
       >
         <div class="w-16 w-1/2 flex-shrink-0 pr-4 align-top sm:w-auto">
           <div
@@ -126,12 +128,14 @@ if (magazine != null && newsfeed != null) {
             <FpImage
               v-if="h.icon"
               :src="h.icon"
-              class="float-right h-14 w-[132px] bg-white py-4 px-4"
+              class="float-right h-14 w-[132px] bg-gray-200 py-4 px-4"
             />
           </a>
         </div>
         <div class="mb-[1px] h-14 max-h-14 overflow-y-hidden align-top">
-          <div class="text-lg font-semibold leading-none text-fp-blue sm:text-xl">
+          <div
+            class="text-lg font-semibold leading-none text-fp-newblue sm:text-xl"
+          >
             <a :href="h.link">{{ h.title }}</a>
           </div>
           <div v-if="h.excerpt" class="hidden text-lg sm:block sm:truncate">
