9a73bca89d4c26c60c05a9cb4814feb516cce379
niko <n.dunkel@gmail.com>
2023-04-12 16:45:35 -0700

    mobile fix

--- components/ui/TheFooter.vue
+++ components/ui/TheFooter.vue
@@ -34,7 +34,7 @@ const availableLocales = computed(() => {
     <!-- Fedora Section -->
     <section class="container mx-auto py-10">
       <div class="flex flex-wrap items-center gap-4 md:flex-nowrap">
-        <div class="pb-6 ltr:ml-4 rtl:mr-4 md:ml-0 md:w-1/4 md:pb-0">
+        <div class="pb-6 ltr:ml-4 rtl:mr-4 md:ml-0 md:w-1/5 md:pb-0">
           <FpImage
             src="assets/images/logos/fedora-blue.png"
             :alt="$t('Fedora Logo')"
@@ -43,7 +43,7 @@ const availableLocales = computed(() => {
         </div>
         <!-- privacy etc links -->
         <ul
-          class="mx-4 flex grow justify-center gap-4 lg:mx-0 lg:gap-8 xl:justify-start"
+          class="mx-4 flex grow flex-wrap justify-center gap-4 lg:mx-0 lg:gap-8 xl:justify-start"
         >
           <li class="list-none underline underline-offset-1">
             <FpLink
