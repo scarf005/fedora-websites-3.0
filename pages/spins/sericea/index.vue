77c69883110d5166d220daa40afb6d2a50830119
niko <n.dunkel@gmail.com>
2023-04-12 16:59:26 -0700

    fix sericea download links

--- pages/spins/sericea/index.vue
+++ pages/spins/sericea/index.vue
@@ -10,9 +10,9 @@ data._value.descriptionMd = await mdparser(data._value.description);
         light: 'assets/images/spins/spins-Sway-logo-light.png',
         dark: 'assets/images/spins/spins-Sway-logo-dark.png',
       }"
-      home="/immutable/sericea"
+      home="/spins/sericea"
       textColor="text-fp-newblue"
-      :items="[{ name: 'Download', link: '/immutable/sericea/download' }]"
+      :items="[{ name: 'Download', link: '/spins/sericea/download' }]"
     />
 
     <!-- TITLE -->
