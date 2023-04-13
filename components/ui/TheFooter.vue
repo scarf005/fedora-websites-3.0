33d16a2bab2ac2f47fe2fbc710e5f23b8fe65c82
Niko <n.dunkel@gmail.com>
2023-04-13 23:29:52 +0000

    remove translation insanity from footer

--- components/ui/TheFooter.vue
+++ components/ui/TheFooter.vue
@@ -74,13 +74,13 @@ const availableLocales = computed(() => {
             >
           </li>
           <li class="list-none text-fp-gray-darkest">
-            {{ $t("Help improve this website on the ") }}
+            Help improve this website on the 
             <FpLink
               href="https://fedora.gitlab.io/websites-apps/fedora-websites/fedora-websites-3.0/admin/"
               class="underline underline-offset-1 transition duration-300 ease-in-out hover:text-fp-gray dark:text-fp-gray-light dark:hover:text-fp-gray-dark"
               >{{ $t("CMS") }}</FpLink
             >
-            {{ $t("or the ") }}
+            or the 
             <FpLink
               href="https://gitlab.com/fedora/websites-apps/fedora-websites/fedora-websites-3.0"
               class="underline underline-offset-1 transition duration-300 ease-in-out hover:text-fp-gray dark:text-fp-gray-light dark:hover:text-fp-gray-dark"
