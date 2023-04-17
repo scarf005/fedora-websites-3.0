48612d042bcd9ef0e6113be3b26d0fef975ce656
Francois Andrieu <darknao@fedoraproject.org>
2023-04-17 16:50:52 +0200

    add i18n tags to the footer component

--- components/partials/FooterSection.vue
+++ components/partials/FooterSection.vue
@@ -19,38 +19,42 @@ const mobileToggle = ref(false);
         class="flex w-full items-center justify-between transition duration-150 ease-in-out hover:bg-blue-200 sm:hidden"
         @click.prevent="mobileToggle = !mobileToggle"
       >
-        <h4 class="text-2xl font-semibold">{{ title }}</h4>
+        <h4 class="text-2xl font-semibold">{{ $t(title) }}</h4>
         <div class="sm:hidden">
           <Icon name="fa6-solid:chevron-right" />
         </div>
       </button>
       <div class="hidden sm:block">
-        <h4 class="text-2xl font-semibold">{{ title }}</h4>
+        <h4 class="text-2xl font-semibold">{{ $t(title) }}</h4>
       </div>
     </header>
     <ul :class="!mobileToggle ? 'hidden' : 'block'">
       <li v-for="link in links" :key="link.id">
-        <FpLink :href="link.path" v-if="!link.i18n">{{ link.label }}</FpLink>
+        <FpLink :href="link.path" v-if="!link.i18n">{{
+          $t(link.label)
+        }}</FpLink>
         <a
           v-if="link.i18n"
           :href="`${
             $config.app.baseURL.replace(new RegExp('/$'), '') + link.path
           }`"
         >
-          {{ link.label }}
+          {{ $t(link.label) }}
         </a>
       </li>
     </ul>
     <ul class="hidden sm:block">
       <li v-for="link in links" :key="link.id">
-        <FpLink :href="link.path" v-if="!link.i18n">{{ link.label }}</FpLink>
+        <FpLink :href="link.path" v-if="!link.i18n">{{
+          $t(link.label)
+        }}</FpLink>
         <a
           v-if="link.i18n"
           :href="`${
             $config.app.baseURL.replace(new RegExp('/$'), '') + link.path
           }`"
         >
-          {{ link.label }}
+          {{ $t(link.label) }}
         </a>
       </li>
     </ul>
