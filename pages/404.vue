b65065b0cdf2d940a521041ee86bdd1d15808f9a
Francois Andrieu <darknao@fedoraproject.org>
2023-07-11 17:39:41 +0200

    404 updated

--- pages/404.vue
+++ pages/404.vue
@@ -3,12 +3,261 @@
     class="bg-gradient-to-b from-white to-fp-newblue-100 py-12 dark:from-fp-darkblue-500 dark:to-neutral-900 md:py-36"
   >
     <div
-      class="container mx-auto flex max-w-7xl flex-row flex-wrap justify-center gap-16 lg:flex-nowrap"
+      class="container relative mx-auto flex max-w-7xl flex-row flex-wrap justify-center gap-16 lg:flex-nowrap"
     >
-      <div
-        class="flex w-[200px] flex-none shrink items-center justify-center sm:shrink-0 md:w-[400px]"
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="absolute left-10 z-10 hidden animate-[float_4s_ease-in-out_infinite_alternate] md:-top-20 md:block 2xl:left-0"
+        xmlns:vectornator="http://vectornator.io"
+        xml:space="preserve"
+        width="62.757"
+        height="72.953"
+        stroke-miterlimit="10"
+        style="
+          clip-rule: evenodd;
+          fill-rule: nonzero;
+          stroke-linecap: round;
+          stroke-linejoin: round;
+        "
       >
-        <FpImage class="max-h-96" src="public/assets/images/panda_404.png" />
+        <path
+          vectornator:layerName="Curve 5"
+          fill="none"
+          stroke="#000"
+          stroke-width="4.237"
+          d="M30.47.621S21.684 31.509.621 50.669c23.198 15.413 21.438 16.001 33.32 21.663 11.768-7.343 25.997-31.701 28.195-47.048C49.56 13.561 30.47.621 30.47.621Z"
+          style="
+            clip-rule: evenodd;
+            fill: #efefef;
+            fill-opacity: 1;
+            fill-rule: nonzero;
+            stroke: #8c8c8c;
+            stroke-width: 1.24201;
+            stroke-linecap: round;
+            stroke-linejoin: round;
+            stroke-dasharray: none;
+          "
+        />
+      </svg>
+
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="absolute top-0 z-10 hidden animate-[float_4s_ease-in-out_infinite_alternate] md:block"
+        xmlns:vectornator="http://vectornator.io"
+        xml:space="preserve"
+        width="76.938"
+        height="32.175"
+        stroke-miterlimit="10"
+        style="
+          clip-rule: evenodd;
+          fill-rule: nonzero;
+          stroke-linecap: round;
+          stroke-linejoin: round;
+        "
+      >
+        <g
+          fill="none"
+          stroke="#000"
+          stroke-width="4.237"
+          style="
+            clip-rule: evenodd;
+            fill-rule: nonzero;
+            stroke-linecap: round;
+            stroke-linejoin: round;
+          "
+        >
+          <path
+            vectornator:layerName="Curve 3"
+            d="m127.587 378.875 21.181 5.026s-21.712-67.64 4.269-86.226c-37.95 33.572-25.45 81.2-25.45 81.2z"
+            style="
+              fill: #efefef;
+              fill-opacity: 1;
+              stroke: #8c8c8c;
+              stroke-width: 1.41478;
+              stroke-dasharray: none;
+            "
+            transform="matrix(0 .87788 -.87788 0 337.642 -109.664)"
+          />
+          <path
+            vectornator:layerName="Curve 4"
+            d="m140.358 333.404 20.504-13.969-7.714-21.69s-12.236 4.446-12.79 35.659z"
+            style="
+              fill: #efefef;
+              fill-opacity: 1;
+              stroke: #8c8c8c;
+              stroke-width: 1.41478;
+              stroke-dasharray: none;
+            "
+            transform="matrix(0 .87788 -.87788 0 337.642 -109.664)"
+          />
+        </g>
+      </svg>
+
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="absolute top-28 right-10 z-10 hidden animate-[float_5s_ease-in-out_infinite_alternate] lg:block 2xl:right-0"
+        xmlns:vectornator="http://vectornator.io"
+        xml:space="preserve"
+        width="27.916"
+        height="65.519"
+        stroke-miterlimit="10"
+        style="
+          clip-rule: evenodd;
+          fill-rule: nonzero;
+          stroke-linecap: round;
+          stroke-linejoin: round;
+        "
+      >
+        <path
+          vectornator:layerName="Curve 10"
+          fill="none"
+          stroke="#000"
+          stroke-width="4.237"
+          d="M27.29 16.033C24.935 13.149 19.287 7.683 8.41.62 8.142 12.914 15.231 35.568.621 51.788c2.707 2.812 7.52 7.744 12.277 13.11 7.867-15.412 14.618-27.193 14.391-48.865z"
+          style="
+            clip-rule: evenodd;
+            fill: #efefef;
+            fill-opacity: 1;
+            fill-rule: nonzero;
+            stroke: #8c8c8c;
+            stroke-width: 1.24201;
+            stroke-linecap: round;
+            stroke-linejoin: round;
+            stroke-dasharray: none;
+          "
+        />
+      </svg>
+
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="absolute bottom-0 right-40 z-0 hidden animate-[float_6s_ease-in-out_infinite_alternate-reverse] md:block"
+        xmlns:vectornator="http://vectornator.io"
+        xml:space="preserve"
+        width="80.95"
+        height="75.764"
+        stroke-miterlimit="10"
+        style="
+          clip-rule: evenodd;
+          fill-rule: nonzero;
+          stroke-linecap: round;
+          stroke-linejoin: round;
+        "
+      >
+        <path
+          vectornator:layerName="Curve 6"
+          fill="none"
+          stroke="#000"
+          stroke-width="4.237"
+          d="M46.268.621C43.765 14.868 25.728 42.777.62 42.14c18.886 15.487 17.416 19.384 32.047 33.003 15.444-7.373 35.808-15.36 47.661-46.258C75.94 21.161 49.52 3.598 46.268.621Z"
+          style="
+            clip-rule: evenodd;
+            fill: #efefef;
+            fill-opacity: 1;
+            fill-rule: nonzero;
+            stroke: #8c8c8c;
+            stroke-width: 1.24201;
+            stroke-linecap: round;
+            stroke-linejoin: round;
+            stroke-dasharray: none;
+          "
+        />
+      </svg>
+
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="absolute bottom-0 left-64 z-30 hidden animate-[float_4s_ease-in-out_infinite_alternate] md:block"
+        xmlns:vectornator="http://vectornator.io"
+        xml:space="preserve"
+        width="75.6"
+        height="58.485"
+        stroke-miterlimit="10"
+        style="
+          clip-rule: evenodd;
+          fill-rule: nonzero;
+          stroke-linecap: round;
+          stroke-linejoin: round;
+        "
+      >
+        <path
+          vectornator:layerName="Curve 7"
+          fill="none"
+          stroke="#000"
+          stroke-width="4.237"
+          d="M12.976 17.657C44.296 13.216 74.98.62 74.98.62l-2.1 44.083L.622 57.864s5.252-19.942 12.355-40.207z"
+          style="
+            clip-rule: evenodd;
+            fill: #efefef;
+            fill-opacity: 1;
+            fill-rule: nonzero;
+            stroke: #8c8c8c;
+            stroke-width: 1.24201;
+            stroke-linecap: round;
+            stroke-linejoin: round;
+            stroke-dasharray: none;
+          "
+        />
+      </svg>
+
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="absolute bottom-24 left-24 z-10 hidden animate-[float_4s_ease-in-out_infinite_alternate-reverse] lg:block"
+        xmlns:vectornator="http://vectornator.io"
+        xml:space="preserve"
+        width="72.317"
+        height="31.98"
+        stroke-miterlimit="10"
+        style="
+          clip-rule: evenodd;
+          fill-rule: nonzero;
+          stroke-linecap: round;
+          stroke-linejoin: round;
+        "
+      >
+        <g
+          fill="none"
+          stroke="#000"
+          stroke-width="4.237"
+          style="
+            clip-rule: evenodd;
+            fill-rule: nonzero;
+            stroke-linecap: round;
+            stroke-linejoin: round;
+          "
+        >
+          <path
+            vectornator:layerName="Curve 8"
+            d="m469.472 511.285 12.93-50.116s40.749-4.486-11.077-30.412c-2.369-1.185 13.277 8.654-8.711 62.257 2.011.466 10.623 3.932 10.623 3.932z"
+            style="
+              fill: #efefef;
+              fill-opacity: 1;
+              stroke: #8c8c8c;
+              stroke-width: 1.41478;
+              stroke-dasharray: none;
+            "
+            transform="matrix(0 .87788 -.87788 0 449.77 -405.5)"
+          />
+          <path
+            vectornator:layerName="Curve 9"
+            d="M469.55 511.343s12.155 2.05 14.195-4.869c3.144-10.66 13.883-53.721 13.883-53.721-4.315 5.46-8.088 7.124-15.145 8.327z"
+            style="
+              fill: #efefef;
+              fill-opacity: 1;
+              stroke: #8c8c8c;
+              stroke-width: 1.41478;
+              stroke-dasharray: none;
+            "
+            transform="matrix(0 .87788 -.87788 0 449.77 -405.5)"
+          />
+        </g>
+      </svg>
+
+      <div
+        class="flex w-[200px] flex-none shrink items-center justify-center sm:shrink-0 md:w-[330px] lg:w-[400px]"
+      >
+        <FpImage
+          class="z-20 max-h-96"
+          src="public/assets/images/panda_404.png"
+        />
       </div>
       <div class="max-w-sm">
         <h2
@@ -29,7 +278,7 @@
         <p class="text-center">
           <FpLink
             href="/"
-            class="rounded-md bg-fp-newblue-500 px-6 py-2 text-xl font-medium text-white duration-300 ease-in-out hover:bg-fp-blue dark:bg-fp-darkblue-500 dark:hover:bg-fp-blue"
+            class="relative z-20 rounded-md bg-fp-newblue-500 px-6 py-2 text-xl font-medium text-white duration-300 ease-in-out hover:bg-fp-blue dark:bg-fp-darkblue-500 dark:hover:bg-fp-blue"
             >{{ $t("Go Home") }}</FpLink
           >
         </p>
@@ -37,3 +286,14 @@
     </div>
   </section>
 </template>
+
+<style>
+@keyframes float {
+  0% {
+    transform: translateY(-0.2rem) rotate(-5deg) translateX(-0.2rem);
+  }
+  100% {
+    transform: translateY(0.2rem) rotate(5deg) translateX(0.2rem);
+  }
+}
+</style>
