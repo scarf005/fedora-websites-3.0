11c54882a2ccbc9db045bd482e1dd4a0ce3c9dff
Francois Andrieu <darknao@fedoraproject.org>
2023-10-09 21:52:58 +0200

    disable js on translated pages (among others)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -86,10 +86,18 @@ export default defineNuxtConfig({
   },
   routeRules: {
     "*": { experimentalNoScripts: true }, // one level deep, render all pages statically
+    "*/*/": { experimentalNoScripts: true }, // same, but for translated pages
+    "spins/": { experimentalNoScripts: true }, // no js on spins home page
+    "*/spins/": { experimentalNoScripts: true }, // no js on spins home page - translated
+    "spins/*/": { experimentalNoScripts: true }, // no js on spins pages either
+    "*/spins/*/": { experimentalNoScripts: true }, // no js on spins pages either - translated
     "*/community/" : { experimentalNoScripts: true }, // no js for community pages
+    "*/*/community/" : { experimentalNoScripts: true }, // no js for community pages - translated
     "/coreos/download/": { experimentalNoScripts: false }, // except the download pages, where we need JS (coreOS images, or just GPG modal)
     "/coreos/release-notes/": { experimentalNoScripts: false }, // except release notes
     "/start": { experimentalNoScripts: false }, // except start pages
+    "*/start": { experimentalNoScripts: false }, // except start pages - translated
     "/security": { experimentalNoScripts: false }, // except security page, for obsolete keys listing
+    "*/security": { experimentalNoScripts: false }, // except security page, for obsolete keys listing - translated
   },
 });
