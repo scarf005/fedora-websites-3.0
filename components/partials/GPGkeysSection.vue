1f5c0a47bdc9ae638f793596d855d7469f572068
Gregory Bartholomew <gregory.lee.bartholomew@gmail.com>
2023-11-13 20:32:51 +0000

    security: make gpg keys match with spaces

--- components/partials/GPGkeysSection.vue
+++ components/partials/GPGkeysSection.vue
@@ -20,24 +20,38 @@ function splitkey(key) {
       <h3 class="pb-3 text-center text-fp-blue sm:text-start">
         {{ gpg.name }}
       </h3>
-      <p>
-        <span class="font-bold">id: </span
-        ><code class="text-xs text-fp-gray sm:text-base">{{ gpg.id }}</code>
-      </p>
-      <p class="flex flex-wrap items-baseline gap-x-1">
-        <span class="font-bold">Fingerprint: </span>
-        <code class="break-all text-xs text-fp-gray sm:text-base">
-          <span class="pr-2" v-for="kp in splitkey(gpg.fingerprint)">
-            {{ kp }}
-          </span>
-        </code>
-      </p>
-      <p v-if="gpg.openpgpkey" class="flex flex-wrap items-baseline gap-x-1">
-        <span class="font-bold">DNS OpenPGPKey: </span
+      <div>
+        <span class="font-bold">id:</span
+        ><code class="ml-[1.25ex] text-xs text-fp-gray sm:text-base">{{
+          gpg.id
+        }}</code>
+      </div>
+      <div class="flex flex-wrap items-baseline">
+        <span class="font-bold">Fingerprint:</span>
+        <div
+          class="ml-[1.25ex] inline relative text-xs text-fp-gray sm:text-base"
+        >
+          <code class="-ml-[1.25ex] absolute text-transparent">
+            <span class="ml-[1.25ex]" v-for="kp in splitkey(gpg.fingerprint)">
+              {{ kp }}
+            </span>
+          </code>
+          <code class="-ml-[1.25ex] select-none">
+            <span class="ml-[1.25ex]">
+              {{ splitkey(gpg.fingerprint).join(" ") }}
+            </span>
+          </code>
+        </div>
+      </div>
+      <div
+        v-if="gpg.openpgpkey"
+        class="flex flex-wrap items-baseline gap-x-[1.25ex]"
+      >
+        <span class="font-bold">DNS OpenPGPKey:</span
         ><code class="break-all text-xs text-fp-gray sm:text-base">{{
           gpg.openpgpkey
         }}</code>
-      </p>
+      </div>
     </div>
   </div>
 </template>
