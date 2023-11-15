d3ea7ed7e18a57e6071f7cd88e55c7f14f4dcfac
Gregory Bartholomew <gregory.lee.bartholomew@gmail.com>
2023-11-15 20:03:55 +0000

    atomic: add landing page

---
+++ components/partials/AtomicDesktopsPreviewSection.vue
@@ -0,0 +1,46 @@
+<script setup>
+const { t } = useI18n();
+const slots = useSlots();
+const props = defineProps({
+  name: {
+    type: String,
+  },
+});
+</script>
+
+<template>
+  <div :class="`mx-auto grid max-w-screen-xl grid-cols-1 md:grid-cols-2`">
+    <div class="col-span-1 flex items-center justify-center p-5">
+      <FpImage
+        :src="`assets/images/atomic-desktops/screenshot-${name}.jpg`"
+        class="max-h-72 rounded-lg border border-neutral-900 drop-shadow-lg"
+      />
+    </div>
+    <div
+      :class="`col-span-1 flex flex-col items-center justify-center p-5 text-center sm:text-start md:items-start`"
+    >
+      <FpImage
+        :src="{
+          light: `assets/images/fedora-${name}-logo-light.png`,
+          dark: `assets/images/fedora-${name}-logo-dark.png`,
+        }"
+        class="w-max"
+      />
+      <p class="pt-4 pb-12 font-normal text-gray-600 dark:text-gray-300">
+        <slot></slot>
+      </p>
+      <div class="flex gap-4">
+        <FpBtn
+          :href="`/${name}`"
+          class="!rounded-md !px-10 !py-4 font-display text-sm font-bold"
+          >{{ $t("Get Started") }}</FpBtn
+        >
+        <FpBtn
+          :href="`/${name}/download`"
+          class="!rounded-md !px-10 !py-4 font-display text-sm font-bold"
+          >{{ $t("Download Now") }}</FpBtn
+        >
+      </div>
+    </div>
+  </div>
+</template>
---
+++ pages/atomic-desktops/index.vue
@@ -0,0 +1,64 @@
+<script setup>
+const release_data = await getCMS("release");
+import navigation from "../../config/navigation.js";
+
+useHead({
+  title: "Fedora Atomic Desktops",
+});
+</script>
+<template>
+  <main class="dark:bg-neutral-800">
+    <section
+      class="bg-gradient-to-b from-fp-newblue-300/75 to-white px-2 pb-12 text-center dark:bg-none lg:text-start"
+    >
+      <TheLocalBar
+        :image="{
+          light: `assets/images/fedora-blue.png`,
+          dark: `assets/images/fedora-blue.png`,
+        }"
+        home="/atomic-desktops/"
+        textColor="text-fp-newblue"
+      />
+
+      <!-- TITLE -->
+
+      <div class="container mx-auto max-w-7xl px-2 pt-12">
+        <h1 class="text-4xl text-gray-600 dark:text-gray-200">
+          {{ $t("Atomic Desktops for Fedora.") }}
+        </h1>
+        <div class="flex flex-col flex-wrap gap-8 lg:flex-row">
+          <p class="mt-8 text-gray-600 dark:text-fp-gray-light">
+            {{
+              $t(
+                "Find a Fedora atomic desktop to enhance your experience! You can find the latest versions of our atomic desktops here.",
+              )
+            }}
+          </p>
+          <div
+            class="mx-auto flex h-32 w-32 flex-col items-center justify-center rounded-full bg-fp-newblue-500 p-3 text-white dark:bg-fp-newblue-700 md:h-48 md:w-48 md:p-10"
+          >
+            <p class="font-display text-xs font-semibold md:text-sm">
+              {{ $t("Latest release") }}
+            </p>
+            <p class="font-display text-6xl font-bold md:text-8xl">
+              {{ release_data.ga.releasever }}
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="space-y-24 py-24">
+      <AtomicDesktopsPreviewSection
+        v-for="atomic_desktop in navigation.downloads.sections.find(
+          ({ label }) => label === 'Immutable Desktops',
+        ).links"
+        :name="`${atomic_desktop.path.split('/')[1]}`"
+      >
+        {{ atomic_desktop.description }}
+      </AtomicDesktopsPreviewSection>
+    </section>
+  </main>
+</template>
+
+<style></style>
