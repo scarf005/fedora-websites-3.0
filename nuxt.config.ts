b2e9efbd60f7e489cf7f8cba3245306115ca3ff6
Francois Andrieu <darknao@fedoraproject.org>
2024-02-17 10:36:26 +0100

    Remove pages:extend hook
    Those redirects take precedence over `navigateTo` redirects (which we use
    for atomic-desktops) and create redirect loops on GitLab pages.
    This was needed to preview pages on MR that end with "/index.html".
    It seems like it's no longer the case, so we can remove it.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -63,19 +63,19 @@ export default defineNuxtConfig({
     },
   },
   hooks: {
-    "pages:extend"(pages) {
-      if (process?.env?.CI) {
-        pages.forEach((page) => {
-          pages.push({
-            name: `${page.name}-alias`,
-            path:
-              page.path.length > 1 ? `${page.path}/index.html` : "/index.html",
-            redirect: page.path,
-            file: page.file,
-          });
-        });
-      }
-    },
+    // "pages:extend"(pages) {
+    //   if (process?.env?.CI_JOB_NAME == "build_preview") {
+    //     pages.forEach((page) => {
+    //       pages.push({
+    //         name: `${page.name}-alias`,
+    //         path:
+    //           page.path.length > 1 ? `${page.path}/index.html` : "/index.html",
+    //         redirect: page.path,
+    //         file: page.file,
+    //       });
+    //     });
+    //   }
+    // },
    },
   nitro: {
     prerender: {
