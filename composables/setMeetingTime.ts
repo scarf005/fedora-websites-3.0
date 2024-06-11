933388af5a4e1397d75a2819f9dbd35e3acf0000
Gregory Bartholomew <gregory.lee.bartholomew@gmail.com>
2024-06-11 19:29:26 +0000

    use weblate variable format in meeting time strings

--- composables/setMeetingTime.ts
+++ composables/setMeetingTime.ts
@@ -1,13 +1,11 @@
 // vim:set ts=2 sw=2 et:
 
 export const setMeetingTime = (target, data) => {
-  const { locale, t } = useI18n();
+  const { d, t } = useI18n();
 
   if (!target.description) {
     return;
   }
-
-  target.description = t(target.description);
 
   let count = data.meetings?.length;
   if (!(count > 0 && count <= 3)) {
@@ -18,6 +16,8 @@ export const setMeetingTime = (target, data) => {
   // use the times from the first meeting and discard the rest.
   data = data.meetings[0];
 
+  let _cadence = '$cadence';
+  let _time = '$time';
   if (data.meeting_date && data.meeting_time_start && data.meeting_timezone) {
     let date = new Date(
       `${data.meeting_date}T${data.meeting_time_start}Z`
@@ -25,16 +25,11 @@ export const setMeetingTime = (target, data) => {
     let day = date.toLocaleString('en-US', { "weekday": "long" }).toLowerCase();
     // bi-weekly meetings are signaled by providing fewer than three meetings
     // in the original data set.
-    target.description = target.description.replace(
-      "$cadence", (count == 3) ? t('every_' + day) : t('every_other_' + day)
-    );
-    target.description = target.description.replace(
-      "$time", date.toLocaleString(
-        locale.value, { "timeStyle": "short", "hour12": false, "timeZone": "UTC" }
-      )
-    );
+    _cadence = (count == 3) ? t('every_' + day) : t('every_other_' + day);
+    _time = d(date, { "timeStyle": "short", "hour12": false, "timeZone": "UTC" });
   }
 
+  let _location = '$location';
   if (data.meeting_location) {
     let match;
     let place = data.meeting_location;
@@ -60,6 +55,8 @@ export const setMeetingTime = (target, data) => {
       // https://stackoverflow.com/a/59700714 (CC BY-SA 4.0)
       place = place.replace("@", "\u200B[]()@");
     }
-    target.description = target.description.replace("$location", place);
+    _location = place;
   }
+
+  target.description = t(target.description, { cadence: _cadence, time: _time, location: _location });
 }
