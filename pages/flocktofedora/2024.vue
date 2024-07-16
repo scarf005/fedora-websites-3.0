c72ca6cf3f37ffd41ed8a70902cd1d6ae1c64070
Adrian Edwards <moralcode@fedoraproject.org>
2024-07-16 15:27:37 -0400

    add FAQ's section

--- pages/flocktofedora/2024.vue
+++ pages/flocktofedora/2024.vue
@@ -364,6 +364,20 @@ function formatDate(dateStr) {
       </FpHero>
     </section> -->
 
+    <!-- FAQ -->
+    <section class="my-10 w-10/12" v-if="data.faq.enabled">
+      <header
+        class="mx-auto max-w-screen-xl text-center md:w-11/12 md:text-left"
+      >
+        <h2 class="mb-4 text-center font-medium text-fp-blue">
+          {{ $t(data.faq.sectionTitle) }}
+        </h2>
+      </header>
+      <FpList columns="sm:grid-cols-1" :disableDots="true">
+        <FpListItemDetails v-for="item in data.faq.content" v-bind="item" />
+      </FpList>
+    </section>
+
     <!-- Our Sponsors -->
     <section class="mx-auto w-full py-10 dark:bg-slate-900">
       <h2
