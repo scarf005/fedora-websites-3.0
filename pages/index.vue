73427d79790726825456230c0970f080d355aa10
Adrian Edwards <moralcode@fedoraproject.org>
2024-07-16 14:32:33 -0400

    NuxtLink to FpLink

--- pages/index.vue
+++ pages/index.vue
@@ -216,10 +216,10 @@ useHead({
           </div>
           <div class="text-center">
             <p class="mb-2">{{ $t(card.description) }}</p>
-            <NuxtLink
-              :to="card.link.url"
+            <FpLink
+              :href="card.link.url"
               class="font-semibold underline-offset-1 transition duration-150 ease-in-out hover:text-fp-darkblue hover:underline hover:dark:text-fp-newblue"
-              >{{ $t(card.link.text) }}</NuxtLink
+              >{{ $t(card.link.text) }}</FpLink
             >
           </div>
         </article>
