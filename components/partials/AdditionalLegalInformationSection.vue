df6ab347c65184eaa23d1e5603f4e3713755efb7
Adrian Edwards <moralcode@fedoraproject.org>
2024-07-17 10:05:33 -0400

    add image attributions at the bottom of the site

---
+++ components/partials/AdditionalLegalInformationSection.vue
@@ -0,0 +1,27 @@
+<script setup>
+defineProps({
+  description: {
+    type: String,
+  },
+  // isMarkdown: {
+  //   default: false,
+  //   type: Boolean,
+  // },
+});
+
+// if (isMarkdown) {
+
+// }
+</script>
+<template>
+  <div class="container mx-auto max-w-7xl">
+    <div class="p-2">
+      <ContentRenderer
+        class="markdown text-center dark:text-white"
+        tag="div"
+        v-if="description"
+        :value="description"
+      />
+    </div>
+  </div>
+</template>
--- pages/flocktofedora/2024.vue
+++ pages/flocktofedora/2024.vue
@@ -14,6 +14,16 @@ function getImportantDateColor(status) {
   // default
   return "#dedede";
 }
+
+function getAttributions(items) {
+  return items.filter((value) => value.attribution);
+}
+
+const headerAttributions = await Promise.all(
+  getAttributions(data._value.header_images).map(
+    async (value) => await mdparser(value.attribution),
+  ),
+);
 </script>
 <template>
   <FpHero
@@ -416,6 +426,14 @@ function getImportantDateColor(status) {
       <FpList columns="sm:grid-cols-2">
         <FpListItem v-for="item in data.sponsoring.content" v-bind="item" />
       </FpList>
+    </section>
+
+    <section class="py-12 px-4 dark:bg-black" v-if="headerAttributions">
+      <AdditionalLegalInformationSection
+        v-for="attribution in headerAttributions"
+        :description="attribution"
+        :isMarkdown="true"
+      />
     </section>
   </main>
 </template>
