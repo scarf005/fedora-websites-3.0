3ca84d49b19f37c70e4fa070056edd1d7b8bf8a8
Adrian Edwards <moralcode@fedoraproject.org>
2024-07-18 11:01:31 -0400

    add some different backgrounds to the sections every once in a while

--- pages/flocktofedora/2024.vue
+++ pages/flocktofedora/2024.vue
@@ -102,7 +102,7 @@ const sponsorLevels = [
 
   <main class="flex flex-col items-center dark:bg-black">
     <!-- TODO: Important Dates -->
-    <section class="mx-auto w-full py-10">
+    <section class="mx-auto w-full py-10 dark:bg-slate-900">
       <h2
         class="mx-auto mb-8 max-w-max bg-gradient-to-r from-fp-purple via-fp-newblue-500 to-fp-purple bg-clip-text text-center text-3xl font-bold text-transparent sm:text-5xl lg:text-7xl"
       >
