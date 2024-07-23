8bc62eedfaa22ce64192683edaa3f6db5c34a776
Adrian Edwards <moralcode@fedoraproject.org>
2024-07-23 13:48:12 -0400

    move hero into a component specifically for the flock pages

--- pages/flock/2024.vue
+++ pages/flock/2024.vue
@@ -60,37 +60,7 @@ const sponsorLevels = [
 ];
 </script>
 <template>
-  <FpHero
-    :background="data.header_images[1].image"
-    alignment="bg-top"
-    class="bg-gray-500 bg-blend-multiply"
-  >
-    <FpBanner
-      :logo="data.header_images[0].image"
-      color="text-fp-purple"
-      border="border border-fp-purple"
-      background="text-white bg-fp-purple"
-      :ctas="data.links"
-    >
-      <template #title>
-        <div
-          class="mx-auto sm:-mt-10 max-w-xs text-right text-lg font-semibold uppercase text-white -mt-8 sm:max-w-lg sm:text-2xl"
-        >
-          {{ $t(data.location) }}
-        </div>
-      </template>
-      <template #subtitle>
-        <h2
-          class="mt-5 font-semibold text-white sm:mt-24 text-2xl sm:text-4xl lg:text-6xl"
-        >
-          {{ $t(data.title) }}
-        </h2>
-      </template>
-      <h2 class="my-4 text-center font-semibold text-white">
-        {{ $t(data.description) }}
-      </h2>
-    </FpBanner>
-  </FpHero>
+  <FlHero> </FlHero>
 
   <main class="flex flex-col items-center dark:bg-black">
     <!--Important Dates -->
