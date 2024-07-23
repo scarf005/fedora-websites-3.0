d055963d20a622e83d3f8ebbb0318d1444cfcf20
Adrian Edwards <moralcode@fedoraproject.org>
2024-07-23 13:34:34 -0400

    subhead: use white font for accessible contrast

--- pages/flock/2024.vue
+++ pages/flock/2024.vue
@@ -81,7 +81,7 @@ const sponsorLevels = [
       </template>
       <template #subtitle>
         <h2
-          class="mt-5 font-semibold text-fp-blue sm:mt-24 text-2xl sm:text-4xl lg:text-6xl"
+          class="mt-5 font-semibold text-white sm:mt-24 text-2xl sm:text-4xl lg:text-6xl"
         >
           {{ $t(data.title) }}
         </h2>
