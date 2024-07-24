53aa695ec22929de555ac5a2687faf6840a1b632
Adrian Edwards <moralcode@fedoraproject.org>
2024-07-24 10:07:43 -0400

    just put everything through the markdown parser

--- components/partials/FlSectionDescription.vue
+++ components/partials/FlSectionDescription.vue
@@ -3,10 +3,6 @@ const props = defineProps({
   content: {
     type: String,
   },
-  isMarkdown: {
-    type: Boolean,
-    default: false,
-  },
 });
 
 const contentMd = await mdparser(props.content);
@@ -16,12 +12,5 @@ const contentMd = await mdparser(props.content);
     class="markdown text-center dark:text-gray-300 text-gray-600"
     tag="div"
     :value="contentMd"
-    v-if="isMarkdown"
   />
-  <p
-    class="mx-auto max-w-lg text-center font-normal text-gray-600 dark:text-gray-300"
-    v-else="isMarkdown"
-  >
-    {{ $t(content) }}
-  </p>
 </template>
