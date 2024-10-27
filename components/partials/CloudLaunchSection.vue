4aa12a9430471a363b236f9023ba50c75af96286
Francois Andrieu <darknao@fedoraproject.org>
2024-10-27 15:07:48 +0100

    update EC2 region list

--- components/partials/CloudLaunchSection.vue
+++ components/partials/CloudLaunchSection.vue
@@ -1,5 +1,7 @@
 <script setup>
+import { Azure_regions, EC2_regions } from "../../config/cloud.js";
 const { t } = useI18n();
+
 defineProps({
   showBeta: {
     type: Boolean,
@@ -24,7 +26,7 @@ const arches = {
     azId: "x64",
     azSecurityType: "TrustedLaunch",
   },
-  arm64: {
+  aarch64: {
     desc: t("ARM® aarch64-based instances"),
     azId: "Arm64",
     azSecurityType: "Standard",
@@ -34,69 +36,6 @@ const arches = {
 const btnTitles = {
   aws: t("List AWS EC2 region"),
   az: t("List Azure region"),
-};
-
-// TODO: Find a way to share that with the coreos dl page
-const EC2_regions = {
-  "us-east-2": "US East (Ohio)",
-  "us-east-1": "US East (N. Virginia)",
-  "us-west-1": "US West (N. California)",
-  "us-west-2": "US West (Oregon)",
-  "af-south-1": "Africa (Cape Town)",
-  "ap-east-1": "Asia Pacific (Hong Kong)",
-  "ap-south-2": "Asia Pacific (Hyderabad)",
-  "ap-southeast-3": "Asia Pacific (Jakarta)",
-  "ap-south-1": "Asia Pacific (Mumbai)",
-  "ap-northeast-3": "Asia Pacific (Osaka)",
-  "ap-northeast-2": "Asia Pacific (Seoul)",
-  "ap-southeast-1": "Asia Pacific (Singapore)",
-  "ap-southeast-2": "Asia Pacific (Sydney)",
-  "ap-northeast-1": "Asia Pacific (Tokyo)",
-  "ca-central-1": "Canada (Central)",
-  "eu-central-1": "Europe (Frankfurt)",
-  "eu-west-1": "Europe (Ireland)",
-  "eu-west-2": "Europe (London)",
-  "eu-south-1": "Europe (Milan)",
-  "eu-west-3": "Europe (Paris)",
-  "eu-south-2": "Europe (Spain)",
-  "eu-north-1": "Europe (Stockholm)",
-  "eu-central-2": "Europe (Zurich)",
-  "me-south-1": "Middle East (Bahrain)",
-  "me-central-1": "Middle East (UAE)",
-  "sa-east-1": "South America (São Paulo)",
-};
-
-const Azure_regions = {
-  southafricanorth: "(Africa) South Africa North",
-  australiaeast: "(Asia Pacific) Australia East",
-  centralindia: "(Asia Pacific) Central India",
-  eastasia: "(Asia Pacific) East Asia",
-  japaneast: "(Asia Pacific) Japan East",
-  koreacentral: "(Asia Pacific) Korea Central",
-  southeastasia: "(Asia Pacific) Southeast Asia",
-  canadacentral: "(Canada) Canada Central",
-  northeurope: "(Europe) North Europe",
-  uksouth: "(Europe) UK South",
-  westeurope: "(Europe) West Europe",
-  francecentral: "(Europe) France Central",
-  switzerlandnorth: "(Europe) Switzerland North",
-  germanywestcentral: "(Europe) Germany West Central",
-  italynorth: "(Europe) Italy North",
-  norwayeast: "(Europe) Norway East",
-  swedencentral: "(Europe) Sweden Central",
-  polandcentral: "(Europe) Poland Central",
-  spaincentral: "(Europe) Spain Central",
-  brazilsouth: "(South America) Brazil South",
-  mexicocentral: "(Mexico) Mexico Central",
-  uaenorth: "(Middle East) UAE North",
-  israelcentral: "(Middle East) Israel Central",
-  qatarcentral: "(Middle East) Qatar Central",
-  eastus: "(US) East US",
-  eastus2: "(US) East US 2",
-  centralus: "(US) Central US",
-  southcentralus: "(US) South Central US",
-  westus2: "(US) West US 2",
-  westus3: "(US) West US 3",
 };
 
 function updateAMIs(art) {
@@ -114,9 +53,9 @@ function updateAzure(version, arch) {
 }
 
 function AzureLink(regionID, showBeta) {
-  const ImageName = `Fedora-Cloud-${showAzure.value.version}${(showBeta) ? '-Prerelease' : ''}-${
-    arches[showAzure.value.arch].azId
-  }`;
+  const ImageName = `Fedora-Cloud-${showAzure.value.version}${
+    showBeta ? "-Prerelease" : ""
+  }-${arches[showAzure.value.arch].azId}`;
   const communityImage = `/CommunityGalleries/Fedora-5e266ba4-2250-406d-adad-5d73860d958f/Images/${ImageName}`;
 
   const Azure_params = {
--- pages/coreos/download.vue
+++ pages/coreos/download.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { EC2_regions } from "../../config/cloud.js";
 const { t } = useI18n();
 const data = await getCMS("editions/coreos/download");
 const route = useRoute();
@@ -248,35 +249,6 @@ function cloud_arts(data) {
 function getMajor(version) {
   return version.split(".")[0];
 }
-
-const EC2_regions = {
-  "us-east-2": "US East (Ohio)",
-  "us-east-1": "US East (N. Virginia)",
-  "us-west-1": "US West (N. California)",
-  "us-west-2": "US West (Oregon)",
-  "af-south-1": "Africa (Cape Town)",
-  "ap-east-1": "Asia Pacific (Hong Kong)",
-  "ap-south-2": "Asia Pacific (Hyderabad)",
-  "ap-southeast-3": "Asia Pacific (Jakarta)",
-  "ap-south-1": "Asia Pacific (Mumbai)",
-  "ap-northeast-3": "Asia Pacific (Osaka)",
-  "ap-northeast-2": "Asia Pacific (Seoul)",
-  "ap-southeast-1": "Asia Pacific (Singapore)",
-  "ap-southeast-2": "Asia Pacific (Sydney)",
-  "ap-northeast-1": "Asia Pacific (Tokyo)",
-  "ca-central-1": "Canada (Central)",
-  "eu-central-1": "Europe (Frankfurt)",
-  "eu-west-1": "Europe (Ireland)",
-  "eu-west-2": "Europe (London)",
-  "eu-south-1": "Europe (Milan)",
-  "eu-west-3": "Europe (Paris)",
-  "eu-south-2": "Europe (Spain)",
-  "eu-north-1": "Europe (Stockholm)",
-  "eu-central-2": "Europe (Zurich)",
-  "me-south-1": "Middle East (Bahrain)",
-  "me-central-1": "Middle East (UAE)",
-  "sa-east-1": "South America (São Paulo)",
-};
 
 useHead({ htmlAttrs: { class: "scroll-smooth" } });
 useContentHead(data);
